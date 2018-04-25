a45848d8fa86273aa88c333b314cb3b4c1996133
Erika Pauwels <erika.pauwels@gmail.com>
2018-04-25 10:08:08 +0200

    Fix linting error

--- tests/unit/utils/rdfa-backspace-handler-test.js
+++ tests/unit/utils/rdfa-backspace-handler-test.js
@@ -1,7 +1,7 @@
 import rdfaBackspaceHandler from 'dummy/utils/rdfa-backspace-handler';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | rdfa-backspace-handler', function(hooks) {
+module('Unit | Utility | rdfa-backspace-handler', function(/*hooks*/) {
 
   // Replace this with your real tests.
   test('it works', function(assert) {
