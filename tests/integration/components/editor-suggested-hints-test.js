61cb3c593b7806b7ab9b2e76d03474c3d748f64a
Erika Pauwels <erika.pauwels@gmail.com>
2018-10-01 11:21:14 +0200

    Run codemods

--- tests/integration/components/editor-suggested-hints-test.js
+++ tests/integration/components/editor-suggested-hints-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | editor-suggested-hints', function(hooks) {
 
     await render(hbs`{{editor-suggested-hints}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | editor-suggested-hints', function(hooks) {
       {{/editor-suggested-hints}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
