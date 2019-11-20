54567799cf824d02af4078b959027bd25f629325
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-11-20 10:56:02 +0100

    [merge-from-contenteditable] added click-handler

---
+++ addon/utils/ce/handlers/click-handler.js
@@ -0,0 +1,58 @@
+import EmberObject from '@ember/object';
+import { reads } from '@ember/object/computed';
+import HandlerResponse from './handler-response';
+import nextTextNode from './next-text-node';
+import { warn } from '@ember/debug';
+import { isInLumpNode, getNextNonLumpTextNode } from '../lump-node-utils';
+
+/**
+ * Click Handler, a event handler to handle click events.
+ * __Note__: Handling only happens in specific cases, all remaning will be dispatched to default content-editable behaviour.
+ *
+ * @module contenteditable-editor
+ * @class ArrowHandler
+ * @constructor
+ * @extends EmberObject
+ */
+export default EmberObject.extend({
+  currentSelection: reads('rawEditor.currentSelection'),
+
+  /**
+   * tests this handler can handle the specified event
+   * @method isHandlerFor
+   * @param {DOMEvent} event
+   * @return boolean
+   * @public
+   */
+  isHandlerFor(event){
+    return this.isMouseDownInLumpNode(event);
+  },
+
+  isMouseDownInLumpNode(event){
+    return event.type === "mousedown" && isInLumpNode(event.target);
+  },
+
+  /**
+   * handle click event
+   * @method handleEvent
+   * @return {Object} HandlerResponse.create({allowPropagation: false})
+   * @public
+   */
+  handleEvent(event){
+    if(this.isMouseDownInLumpNode(event)) {
+      //Note: now only mousdown in lumpnode is taken care of. All remaining will be default content editable.
+      const nextNode = this.nextNode(event.target);
+      this.rawEditor.updateRichNode();
+      this.rawEditor.setCarret(nextNode, 0);
+    }
+    return new HandlerResponse({ allowPropagation: false });
+  },
+
+  nextNode(current) {
+    let newNode = nextTextNode(current, this.rawEditor.rootNode);
+    if(isInLumpNode(newNode)){
+      return getNextNonLumpTextNode(newNode, this.rawEditor.rootNode);
+    }
+    return newNode;
+  }
+});
