6ae6882f5b0f1410e871a3b38884cd8e711b4c92
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-11-20 10:54:56 +0100

    [merge-from-contenteditable] updated-tab-handler

--- addon/utils/ce/handlers/tab-handler.js
+++ addon/utils/ce/handlers/tab-handler.js
@@ -1,19 +1,26 @@
 import EmberObject from '@ember/object';
 import HandlerResponse from './handler-response';
-import nextTextNode from '../next-text-node';
+import nextTextNode from './next-text-node';
+import { isInLumpNode, getParentLumpNode, getNextNonLumpTextNode } from '../lump-node-utils';
 
 export default EmberObject.extend({
   isHandlerFor(event) {
     return (event.type === "keydown" && event.key === "Tab" && this.rawEditor.currentNode);
   },
+
   handleEvent() {
     const currentNode = this.rawEditor.currentNode;
     const nextNode = this.nextNode(currentNode);
     this.rawEditor.updateRichNode();
     this.rawEditor.setCarret(nextNode, 0);
-    return new HandlerResponse({ allowPropagation: false});
+    return new HandlerResponse({ allowPropagation: false });
   },
+
   nextNode(current) {
-    return nextTextNode(current, this.rawEditor.rootNode);
+    let newNode = nextTextNode(current, this.rawEditor.rootNode);
+    if(isInLumpNode(newNode)){
+      return getNextNonLumpTextNode(newNode, this.rawEditor.rootNode);
+    }
+    return newNode;
   }
 });
