a148e7c3b9744839032a0a69fab12a512267c5e9
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-11-22 11:19:50 +0100

    renamed lumpNodeUri

--- addon/utils/ce/lump-node-utils.js
+++ addon/utils/ce/lump-node-utils.js
@@ -10,17 +10,17 @@ import nextTextNode from './next-text-node';
  * HOW TO USE IT
  * -------------
  * In the HTML-tree, the following would work.
- * <div property='http://lblod.data.gift/vocabularies/editor/isBlockRemovalNode'> whatever content. </div>
+ * <div property='http://lblod.data.gift/vocabularies/editor/isLumpNode'> whatever content. </div>
  *
  * TODO
  * ----
- * - 'property='http://lblod.data.gift/vocabularies/editor/isBlockRemovalNode': no prefixed URI will work.
+ * - 'property='http://lblod.data.gift/vocabularies/editor/isLumpNode': no prefixed URI will work.
  *   This due to performance reasons of MARAWA, which would slow evertything, as long as no incremental changes are supported.
  *  - wiring: there is currently a dichtomy between CE and RDFA editor, and even though this is contained in CE, whilst using RDFA here
  *            this means it probably should not belong here. So location will change.
  */
 
-const LUMP_NODE_URI = 'http://lblod.data.gift/vocabularies/editor/isBlockRemovalNode';
+const LUMP_NODE_URI = 'http://lblod.data.gift/vocabularies/editor/isLumpNode';
 
 function isInLumpNode(node, rootNode){
   if(getParentLumpNode(node, rootNode)){
