64400a41b958b2ef252aa3f281774cd07ba85b92
Niels Vandekeybus <progster@gmail.com>
2019-12-03 13:07:24 +0100

    don't try to highlight empty nodes

--- addon/utils/ce/highlight-property.js
+++ addon/utils/ce/highlight-property.js
@@ -1,4 +1,16 @@
 import EditorProperty from './editor-property';
 
-const highlightProperty = new EditorProperty({attributes: { 'data-editor-highlight': true}});
+class HighlightProperty extends EditorProperty {
+  constructor({attributes = { 'data-editor-highlight': true}})  {
+    super({attributes});
+  }
+  permittedContent(richNode) {
+    const length = richNode.end - richNode.start;
+    if (length > 0 && (richNode.domNode.textContent.trim().length > 0))
+      return [richNode];
+    else
+      return [];
+  }
+}
+const highlightProperty = new HighlightProperty({});
 export default highlightProperty;
