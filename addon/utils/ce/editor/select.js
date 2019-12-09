caf9a86b4623e5db503bb4802def4b68a25e7b9d
Niels Vandekeybus <progster@gmail.com>
2019-12-09 15:26:11 +0100

    fix typos /fp

--- addon/utils/ce/editor/select.js
+++ addon/utils/ce/editor/select.js
@@ -37,11 +37,11 @@ import { analyse as scanContexts } from '@lblod/marawa/rdfa-context-scanner';
 function selectCurrentSelection() {
   // the following line was added to make sure the selection is set correctly before toggling a property
   // if text is selected with the mouse and the mouseUp event happens outside the editor we don't capture it at the moment, this is a workaround
-  this.contentEditable.updateSelectionAfterComplexInput();
+  this.updateSelectionAfterComplexInput();
   const richNode = this.getRichNodeFor(this.currentNode);
   if (this.currentSelection[0] === this.currentSelection[1] && richNode) {
     // it's a collapsed selection, return the currentNode
-    return { selections: { richNode, range: this.currentSelection }, selectedHighlightRange: this.currentSelection  };
+    return { selections: [{ richNode, range: this.currentSelection }], selectedHighlightRange: this.currentSelection  };
   }
   else {
     return selectHighlight.bind(this)(this.currentSelection);
