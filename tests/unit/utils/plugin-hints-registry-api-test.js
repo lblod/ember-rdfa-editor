09863b71782ce386757a1f9a99a7519d2ef828be
Niels Vandekeybus <progster@gmail.com>
2020-04-07 21:01:13 +0200

    add a wrapper around hintsregistry
    two main functions of the wrapper:
    
    1. cleanly separate the api meant for plugins, making documentation easier to
    find
    2. provide the hrId so it no longer needs to be passed to plugins

---
+++ tests/unit/utils/plugin-hints-registry-api-test.js
@@ -0,0 +1,11 @@
+import pluginHintsRegistryApi from 'dummy/utils/plugin-hints-registry-api';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | PluginHintsRegistryApi', function() {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = pluginHintsRegistryApi();
+    assert.ok(result);
+  });
+});
