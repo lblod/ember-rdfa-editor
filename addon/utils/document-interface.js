86794d23b0e917a1c20c0268e2dc4803acfcf3bc
Niels Vandekeybus <progster@gmail.com>
2020-04-07 16:26:22 +0200

    auto update regions for selectHighlight and selectContext

--- addon/utils/document-interface.js
+++ addon/utils/document-interface.js
@@ -12,8 +12,10 @@
  * @constructor
  */
 export default class PluginEditorApi {
-  constructor(editor) {
+  constructor(editor, hintsRegistry, hrId) {
     this._editor = editor;
+    this._hintsregistry = hintsRegistry;
+    this._hrId = hrId;
   }
 
   /**
@@ -64,7 +66,9 @@ export default class PluginEditorApi {
    * on this selection.
    * @public
    */
-  selectHighlight(range, options) { return this._editor.selectHighlight(range, options); }
+  selectHighlight(range, options) {
+    const updatedLocation = this._hintsRegistry.updateLocationToCurrentIndex(this._hrId, range);
+    return this._editor.selectHighlight(updatedLocation, options); }
 
   /**
    * Selects nodes based on an RDFa context that should be applied.
@@ -109,7 +113,9 @@ export default class PluginEditorApi {
    * regular expression of attribute available on the node.
    * @public
    */
-  selectContext(region, options) { return this._editor.selectContext(region, options); }
+  selectContext(region, options) {
+    const updatedLocation = this._hintsRegistry.updateLocationToCurrentIndex(this._hrId, region);
+    return this._editor.selectContext(updatedLocation, options); }
 
   /**
    * Updates a selection as described above.
