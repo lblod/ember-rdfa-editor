b6ebb10905728e046723155d917803f70acaaaf4
Oscar Rodriguez <oscar@verify.as>
2020-06-09 15:59:27 +0200

    clean tab handler

--- addon/utils/ce/handlers/tab-handler.js
+++ addon/utils/ce/handlers/tab-handler.js
@@ -1,18 +1,5 @@
-import EmberObject from '@ember/object';
 import HandlerResponse from './handler-response';
 import nextTextNode from '../next-text-node';
-
-import getRichNodeMatchingDomNode from '../get-rich-node-matching-dom-node';
-import {
-  tagName,
-  isDisplayedAsBlock,
-  invisibleSpace,
-  insertNodeBAfterNodeA,
-  insertTextNodeWithSpace
-} from '../dom-helpers';
-import HandlerResponse from './handler-response';
-import { debug } from '@ember/debug';
-import { isBlank } from '@ember/utils';
 
 /**
  * Enter Handler, a event handler to handle the generic enter case
