f76a93478de9e7a143dc0e070e4b65f4483b7755
Oscar Rodriguez <oscar@verify.as>
2020-06-11 10:49:06 +0200

    removed another console.log I forgot

--- addon/utils/ce/editor/update.js
+++ addon/utils/ce/editor/update.js
@@ -392,7 +392,6 @@ function updateInnerContent(domNodes, {remove, set}) {
  * @method sanitizeHTML
  */
 function sanitizeHTML(html) {
-  console.log(html)
   const safeAttributes = ['colspan', 'rowspan', 'title', 'alt', 'cellspacing', 'axis', 'about', 'property', 'datatype', 'typeof', 'resource', 'rel', 'rev', 'content', 'vocab', 'prefix', 'href', 'src', 'style', 'class'];
   const uriSafeAttr = ['typeof', 'property']
   const inputParser = new HTMLInputParser({lumpTags: [], safeAttributes, uriSafeAttr});
