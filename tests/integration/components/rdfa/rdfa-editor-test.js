c0150c1e2f1790eda1998b3ac914cfa564a4b9ae
Oscar Rodriguez <oscar@verify.as>
2020-07-17 10:46:54 +0200

    remove problematic tests and created a rendering test for the editor

---
+++ tests/integration/components/rdfa/rdfa-editor-test.js
@@ -0,0 +1,25 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | rdfa-editor', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+    // this.on('handleRdfaEditorInit', () => {
+    //   console.log('init');
+    // });
+    await render(hbs`<Rdfa::RdfaEditor
+      @profile="default"
+      class="rdfa-playground"
+      @editorOptions={{hash showToggleRdfaAnnotations="true" showInsertButton=null showRdfa="true" showRdfaHighlight="true" showRdfaHover="true"}}
+      @toolbarOptions={{hash showTextStyleButtons="true" showListButtons="true" showIndentButtons="true"}}
+    />`);
+    assert.dom('.say-editor').exists();
+
+  });
+
+});
