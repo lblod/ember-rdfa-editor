a71caddfb3d1748e919a4a3135b5121364641466
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-07-22 15:33:14 +0200

    [wip] rdfa plugin

--- addon/utils/rdfa/handlers/text-input-data-flagged-remove-handler.js
+++ addon/utils/rdfa/handlers/text-input-data-flagged-remove-handler.js
@@ -1,6 +1,6 @@
 import TextInputHandler from '../../ce/handlers/text-input-handler';
 import HandlerResponse from '../../ce/handlers/handler-response';
-import { isAllWhitespace } from '../../ce/dom-helpers';
+import { isAllWhitespace, invisibleSpace } from '../../ce/dom-helpers';
 
 export default class InputDataRemovedHandler extends TextInputHandler {
   /**
@@ -15,7 +15,7 @@ export default class InputDataRemovedHandler extends TextInputHandler {
     if (currentNode) {
       const parentNode = currentNode.parentNode;
       return super.isHandlerFor(...arguments) &&
-        currentNode.length < 4 &&
+        this.stringToVisibleText(parentNode.innerText).length < 4 &&
         parentNode &&
         parentNode.getAttribute('data-flagged-remove');
     }
@@ -27,7 +27,8 @@ export default class InputDataRemovedHandler extends TextInputHandler {
   handleEvent(){
     //this is the span
     const currentNode = this.rawEditor.currentNode;
-    const length = currentNode.length;
+    const parentNode = currentNode.parentNode;
+    const length = this.stringToVisibleText(parentNode.innerText).length;
     if ( length > 0 && length < 3 ) {
       if (isAllWhitespace(currentNode))
         currentNode.textContent = '';
@@ -36,5 +37,14 @@ export default class InputDataRemovedHandler extends TextInputHandler {
     else
       currentNode.parentNode.removeAttribute('data-flagged-remove');
     return HandlerResponse.create({});
+  }
+
+
+    //TODO: move to util
+  stringToVisibleText(foo) {
+    // \s as per JS [ \f\n\r\t\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff].
+    return foo
+      .replace(invisibleSpace,'')
+      .replace(/[ \f\n\r\t\v\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]+/g,'');
   }
 }
