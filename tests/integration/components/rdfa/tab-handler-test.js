ffdba256a05b4d07e90b174a362cacc45df28397
Oscar Rodriguez <oscar@verify.as>
2020-07-23 17:24:19 +0200

    first tab test

---
+++ tests/integration/components/rdfa/tab-handler-test.js
@@ -0,0 +1,29 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, triggerKeyEvent, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | tab-handler', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('tab works', async function(assert) {
+    this.set('rdfaEditorInit', (editor) => {
+      editor.setHtmlContent('<li>baz</li><li>foo</li>');
+    });
+    await render(hbs`<Rdfa::RdfaEditor
+      @rdfaEditorInit={{action rdfaEditorInit}}
+      @profile="default"
+      class="rdfa-playground"
+      @editorOptions={{hash showToggleRdfaAnnotations="true" showInsertButton=null showRdfa="true" showRdfaHighlight="true" showRdfaHover="true"}}
+      @toolbarOptions={{hash showTextStyleButtons="true" showListButtons="true" showIndentButtons="true"}}
+    />`);
+    var editor = document.querySelector("div[contenteditable]");
+    const bazWordNode = editor.childNodes[0];
+    window.getSelection().collapse(bazWordNode,0);
+    click('div[contenteditable]');
+    await triggerKeyEvent('div[contenteditable]', 'keydown', 'Tab');
+    const cursorPosition = window.getSelection().anchorOffset;
+    assert.equal(cursorPosition, 0);
+  });
+
+});
