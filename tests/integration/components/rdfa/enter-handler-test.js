dc1f386c38549aa83256e0b9db4bbfe71e4a02c9
Oscar Rodriguez <oscar@verify.as>
2020-07-23 17:06:09 +0200

    added li test to the enter handler

--- tests/integration/components/rdfa/enter-handler-test.js
+++ tests/integration/components/rdfa/enter-handler-test.js
@@ -30,4 +30,27 @@ module('Integration | Component | enter-handler', function(hooks) {
     assert.equal(cursorPosition, 0);
   });
 
+  test('enter handler works with li', async function(assert) {
+    this.set('rdfaEditorInit', (editor) => {
+      editor.setHtmlContent('<li>baz</li>');
+    });
+    await render(hbs`<Rdfa::RdfaEditor
+      @rdfaEditorInit={{action rdfaEditorInit}}
+      @profile="default"
+      class="rdfa-playground"
+      @editorOptions={{hash showToggleRdfaAnnotations="true" showInsertButton=null showRdfa="true" showRdfaHighlight="true" showRdfaHover="true"}}
+      @toolbarOptions={{hash showTextStyleButtons="true" showListButtons="true" showIndentButtons="true"}}
+    />`);
+    var editor = document.querySelector("div[contenteditable]");
+    const bazWordNode = editor.childNodes[0];
+    window.getSelection().collapse(bazWordNode,1);
+    click('div[contenteditable]');
+    await triggerKeyEvent('div[contenteditable]', 'keydown', 'Enter');
+
+    const innerHtml = editor.innerHTML;
+    assert.equal(innerHtml, '<li>baz</li><li data-editor-position-level="0"></li>');
+    const cursorPosition = window.getSelection().anchorOffset;
+    assert.equal(cursorPosition, 0);
+  });
+
 });
