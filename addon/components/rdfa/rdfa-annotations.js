8ea4ec5d1f10fa348d0a7577a35d1a429c3e745d
Oscar Rodriguez <oscar@verify.as>
2020-07-31 13:58:23 +0200

    moved trigger to an observer

--- addon/components/rdfa/rdfa-annotations.js
+++ addon/components/rdfa/rdfa-annotations.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import RdfaContextScanner from '@lblod/marawa/rdfa-context-scanner';
 import {tracked} from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency-decorators';
 /**
  * @module rdfa-editor
  * @class RdfaEditorSuggestedHintsComponent
@@ -12,20 +13,31 @@ export default class EditorSuggestedHints extends Component {
   topPositions = {}
   constructor() {
     super(...arguments);
-    this.interval = setInterval(async () => {
-      const cursor = document.querySelector('[data-editor-position-level="0"]');
-      const scanner = new RdfaContextScanner();
-      const rdfaBlocks = scanner.analyse(cursor);
-      this.resetTopPositions();
-      if(rdfaBlocks.length) {
-        let parentArray = this.getParentArray(rdfaBlocks[0].richNodes[0]);
-        parentArray = this.extractLastContext(parentArray);
-        parentArray = parentArray.filter((parent) => parent.lastContext);
-        parentArray = this.addTopPositions(parentArray);
-        parentArray = await this.queryLabels(parentArray);
-        this.rdfaBlocks = parentArray;
-      }
-    }, 5000);
+    setTimeout(() => {
+      const editorPaper = document.querySelector('.say-editor__paper');
+      const callback = () => {
+        this.generateAnnotations.perform();
+      };
+      const observer = new MutationObserver(callback);
+      observer.observe(editorPaper, {attributes: true, subtree: true});
+    }, 250);
+    
+    // this.interval = setInterval(this.generateAnnotations.bind(this), 5000);
+  }
+  @task({ restartable: true })
+  *generateAnnotations(){
+    const cursor = document.querySelector('[data-editor-position-level="0"]');
+    const scanner = new RdfaContextScanner();
+    const rdfaBlocks = scanner.analyse(cursor);
+    this.resetTopPositions();
+    if(rdfaBlocks.length) {
+      let parentArray = this.getParentArray(rdfaBlocks[0].richNodes[0]);
+      parentArray = this.extractLastContext(parentArray);
+      parentArray = parentArray.filter((parent) => parent.lastContext);
+      parentArray = this.addTopPositions(parentArray);
+      parentArray = yield this.queryLabels(parentArray);
+      this.rdfaBlocks = parentArray;
+    }
   }
   getParentArray(startNode) {
     const richNodesOnPath = [startNode];
@@ -46,6 +58,7 @@ export default class EditorSuggestedHints extends Component {
       if(node.rdfaContext && node.rdfaContext.length) {
         node.lastContext = node.rdfaContext[node.rdfaContext.length-1];
       }
+      return node;
     });
   }
   addTopPositions(nodeArray) {
@@ -54,6 +67,7 @@ export default class EditorSuggestedHints extends Component {
         node.hasTopPosition = true;
         node.topPosition = this.blockPlacement(this.calculateNodeOffset(node.domNode) - 96 - 44); // Magic numbers for now, they correspond to the height of the navbar and the toolbar
       }
+      return node;
     });
   }
   calculateNodeOffset(node) {
@@ -72,7 +86,7 @@ export default class EditorSuggestedHints extends Component {
     }
   }
   async queryLabels(nodeArray) {
-    await Promise.all(nodeArray.map(async (node) => {
+    return await Promise.all(nodeArray.map(async (node) => {
       if(node.lastContext.typeof) {
         node.lastContext.typeof[0] = (await this.resourceMetadata.fetch(node.lastContext.typeof[0])).label;
       }
