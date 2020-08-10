b7b65ebd25f6c58303799e6ae9826abc93c60beb
Oscar Rodriguez <oscar@verify.as>
2020-08-10 10:53:38 +0200

    added comments to the helper and the service

--- addon/helpers/sum.js
+++ addon/helpers/sum.js
@@ -1,5 +1,8 @@
 import { helper } from '@ember/component/helper';
 
+/**
+* Helper that returns the sum of all its arguments
+*/
 export function sum(params) {
   return params.reduce((a, b) => {
     if(!b) return a;
--- addon/services/resource-metadata.js
+++ addon/services/resource-metadata.js
@@ -1,5 +1,12 @@
 import Service from '@ember/service';
 
+/**
+* Service in charge of querying the label and comment correspondent to an uri from the backend
+*
+* @module rdfa-editor
+* @class ResourceMetadataService
+* @extends Service
+*/
 export default class ResourceMetadataService extends Service {
   async fetch(uri) {
     const response = await fetch('/resource-labels/getInfo', {
