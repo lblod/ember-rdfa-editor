073f7e969cc5b611cca9a959de1472e0d9947885
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-08-17 14:31:50 +0200

    improved behaviour of tabbing in nested lists

--- addon/utils/plugins/lists/tab-input-plugin.ts
+++ addon/utils/plugins/lists/tab-input-plugin.ts
@@ -1,6 +1,6 @@
 import { TabInputPlugin } from '@lblod/ember-rdfa-editor/editor/input-handlers/tab-handler';
 import { Editor, Manipulation, ManipulationGuidance } from '@lblod/ember-rdfa-editor/editor/input-handlers/manipulation';
-import { isList, isLI, getAllLisFromList, isEmptyList, siblingLis, findLastLi } from '@lblod/ember-rdfa-editor/utils/ce/dom-helpers';
+import { isList, isLI, getAllLisFromList, isEmptyList, siblingLis, findLastLi, tagName } from '@lblod/ember-rdfa-editor/utils/ce/dom-helpers';
 import { indentAction } from '@lblod/ember-rdfa-editor/utils/ce/list-helpers';
 import { stringToVisibleText } from '@lblod/ember-rdfa-editor/editor/input-handlers/backspace-handler'
 
@@ -31,7 +31,10 @@ export default class ListTabInputPlugin implements TabInputPlugin {
         const list = manipulation.node.parentElement;
         const lastLi = findLastLi(list);
 
-        if(lastLi.isSameNode(listItem)){
+        if(lastLi.isSameNode(listItem) && isLiInNestedList(listItem) ){
+          return { allow: true, executor: this.jumpToNextLiOfParentList };
+        }
+        else if( lastLi.isSameNode(listItem) ){
           return { allow: true, executor: this.jumpOutOfList };
         }
         else {
@@ -78,12 +81,39 @@ export default class ListTabInputPlugin implements TabInputPlugin {
       textNode = element.nextSibling;
     }
     else {
-      textNode = document.createTextNode('');
+      textNode = document.createTextNode(invisibleSpace);
       element.after(textNode);
     }
 
     editor.updateRichNode();
     editor.setCarret(textNode, 0);
+  }
+
+  jumpToNextLiOfParentList(manipulation: Manipulation, editor: Editor){
+    const list =  manipulation.node.parentElement;
+
+    if( !list ) throw 'lists/tab-input-plugin expected list to exist';
+    const parentLi = list.parentElement;
+
+    if( !parentLi ) throw 'lists/tab-input-plugin expected parentLi to exist';
+
+    const parentList = parentLi.parentElement;
+    if( !parentList ) throw 'lists/tab-input-plugin expected parentList to exist';
+
+    //start positioning
+    const listItems = siblingLis(parentLi);
+    const indexOfLi = listItems.indexOf(parentLi);
+
+    let nextLi;
+
+    if(indexOfLi == listItems.length - 1){
+      nextLi = document.createElement('li');
+      parentList.append(nextLi);
+    }
+    else {
+      nextLi = listItems[indexOfLi + 1];
+    }
+    setCursorAtBeginningOfElement(nextLi, editor);
   }
 }
 
@@ -98,4 +128,29 @@ function setCursorAtBeginningOfElement(element : HTMLElement, editor: Editor){
   }
   editor.updateRichNode();
   editor.setCarret(textNode, 0);
+}
+
+function isLiInNestedList( li: HTMLElement ) : boolean{
+  const list =  li.parentElement;
+
+  if(!list){
+    return false;
+  }
+
+  const parentLi = list.parentElement;
+
+  if(!parentLi){
+    return false;
+  }
+
+  const parentList = parentLi.parentElement;
+  if(!parentList) {
+    return false;
+  }
+
+  if(tagName(parentList) === 'ul' || tagName(parentList) === 'ol'){
+    return true
+  }
+
+  return false;
 }
