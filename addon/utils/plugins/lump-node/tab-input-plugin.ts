7ec95e310a6e330aeaa2edad8be80c950158b4c0
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-08-18 16:27:18 +0200

    Added shift-tab behavior for lump-node/tab-input-plugin

--- addon/utils/plugins/lump-node/tab-input-plugin.ts
+++ addon/utils/plugins/lump-node/tab-input-plugin.ts
@@ -11,8 +11,13 @@ import { invisibleSpace } from '@lblod/ember-rdfa-editor/utils/ce/dom-helpers';
 export default class LumpNodeTabInputPlugin implements TabInputPlugin {
   label = 'Tap input plugin for handling LumpNodes'
 
+  isSupportedManipulation(manipulation : Manipulation) : boolean {
+    return manipulation.type  === 'moveCursorInsideNonVoidAndVisibleElementAtStart'
+      || manipulation.type  === 'moveCursorInsideNonVoidAndVisibleElementAtEnd';
+  }
+
   guidanceForManipulation(manipulation : Manipulation) : ManipulationGuidance | null {
-    if( manipulation.type !== 'moveCursorInsideNonVoidAndVisibleElementAtStart' ){
+    if( !this.isSupportedManipulation(manipulation) ){
       console.info(`plugins/lump-node/tab-input-plugin: manipulation ${manipulation.type} not supported for lumpNode`);
       return null;
     }
@@ -21,10 +26,16 @@ export default class LumpNodeTabInputPlugin implements TabInputPlugin {
     const rootNode = element.getRootNode(); //Assuming here that node is attached.
     const isElementInLumpNode = isInLumpNode(element, rootNode);
 
-    if(isElementInLumpNode){
+    if(manipulation.type  === 'moveCursorInsideNonVoidAndVisibleElementAtStart' && isElementInLumpNode){
       return {
         allow: true,
         executor: this.jumpOverLumpNode
+      };
+    }
+    else if(manipulation.type  === 'moveCursorInsideNonVoidAndVisibleElementAtEnd' && isElementInLumpNode){
+      return {
+        allow: true,
+        executor: this.jumpOverLumpNodeBackwards
       };
     }
 
@@ -41,12 +52,31 @@ export default class LumpNodeTabInputPlugin implements TabInputPlugin {
     }
 
     else {
-      //Adding invisibleSpace, to make sure that if LI is last node in parent, the user notices cursor jump
+      //Adding invisibleSpace the user notices cursor jump
       //TODO: probably some duplicat logic wit editor.setCarret
       const textNode = document.createTextNode(invisibleSpace);
       element.after(textNode);
       editor.updateRichNode();
       editor.setCarret(textNode, textNode.length);
+    }
+  }
+
+  jumpOverLumpNodeBackwards( manipulation: Manipulation, editor: Editor ) : void {
+    const element = getParentLumpNode(manipulation.node, manipulation.node.getRootNode()) as HTMLElement; //we can safely assume this
+    if(element.previousSibling && element.previousSibling.nodeType == Node.TEXT_NODE){
+      //TODO: what if textNode does contain only invisible white space? Then user won't see any jumps.
+      const textNode = element.previousSibling as Text;
+      editor.updateRichNode();
+      editor.setCarret(textNode, textNode.length);
+    }
+
+    else {
+      //Adding invisibleSpace the user notices cursor jump
+      //TODO: probably some duplicat logic wit editor.setCarret
+      const textNode = document.createTextNode(invisibleSpace);
+      element.before(textNode);
+      editor.updateRichNode();
+      editor.setCarret(textNode, 0);
     }
   }
 }
