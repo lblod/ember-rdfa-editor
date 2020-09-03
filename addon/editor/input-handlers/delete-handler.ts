a23a7537857f67e1df21afc15690fabf8d61bfac
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-09-03 12:41:21 +0200

    document

--- addon/editor/input-handlers/delete-handler.ts
+++ addon/editor/input-handlers/delete-handler.ts
@@ -418,7 +418,7 @@ export default class DeleteHandler implements InputHandler {
     switch( manipulation.type ) {
       case "removeCharacter":
         visualReferencePoint = new MagicSpan(document.createElement('span'), this.rawEditor);
-        //Note: there is an edge case <span> foo </span> where 'foo' is the textNode to manipulate.
+        //Note: there is an edge case <span> f|oo   <magispan></magicspan></span> where 'foo' is the textNode to manipulate.
         // In chrome if you don't add something to the span, this will behave very weirdly
         // There is a test for this.
         visualReferencePoint.span.appendChild(document.createTextNode(invisibleSpace))
