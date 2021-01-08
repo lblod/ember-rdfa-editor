9554645714a88143bbfb027b0d6e4488ec96ed39
Niels Vandekeybus <progster@gmail.com>
2021-01-08 15:48:07 +0100

    use command in the bold italic underline handler

--- addon/utils/ce/handlers/bold-italic-underline-handler.js
+++ addon/utils/ce/handlers/bold-italic-underline-handler.js
@@ -2,6 +2,7 @@ import HandlerResponse from './handler-response';
 import boldProperty from '../../rdfa/bold-property';
 import italicProperty from '../../rdfa/italic-property';
 import underlineProperty from '../../rdfa/underline-property';
+import {PropertyState} from "@lblod/ember-rdfa-editor/utils/ce/model-selection-tracker";
 
 /**
  * BoldItalicUnderlineHandler, a event handler to restore editor state if all else failed
@@ -13,12 +14,25 @@ import underlineProperty from '../../rdfa/underline-property';
 export default class BoldItalicUnderlineHandler {
 
   rawEditor;
+  isBold = false;
+  isItalic = false;
+  isUnderline = false;
+  isStrikethrough = false;
+
   constructor({rawEditor}) {
     this.rawEditor = rawEditor;
+    document.addEventListener("richSelectionUpdated", this.updateProperties.bind(this));
   }
 
   isHandlerFor(event) {
     return event.type == "keydown" && ["b","i","u"].includes(event.key) && (event.ctrlKey || event.metaKey);
+  }
+
+  updateProperties(event) {
+    this.isBold = event.detail.bold === PropertyState.enabled;
+    this.isItalic = event.detail.italic === PropertyState.enabled;
+    this.isUnderline = event.detail.underline === PropertyState.enabled;
+    this.isStrikethrough = event.detail.strikethrough === PropertyState.enabled;
   }
 
 
@@ -26,20 +40,16 @@ export default class BoldItalicUnderlineHandler {
     let property;
     switch(event.key) {
     case "b":
-      property = boldProperty;
+      property = this.isBold ? "remove-bold" : "make-bold";
       break;
     case "u":
-      property = underlineProperty;
+      property = this.isUnderline ? "remove-underline" : "make-underline";
       break;
     case "i":
-      property = italicProperty;
+      property = this.isItalic  ? "remove-italic" : "make-italic";
       break;
     }
-    const range = this.rawEditor.currentSelection;
-    const selection = this.rawEditor.selectCurrentSelection();
-    this.rawEditor.toggleProperty(selection, property);
-    // set cursor at end of selection, TODO: check what other editors do but this feels natural
-    this.rawEditor.setCurrentPosition(range[1]);
+    this.rawEditor.executeCommand(property);
     return HandlerResponse.create({ allowBrowserDefault: false, allowPropagation: false });
   }
 }
