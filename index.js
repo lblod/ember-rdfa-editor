a3b82ec761852099a254a3e5faa6cbf3b6b9fc4f
Dieter Peirs <github@dieterpeirs.com>
2021-01-19 15:08:42 +0100

    include css if sass is not used

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name;
+
+  included() {
+    let hasSass = !!app.registry.availablePlugins['ember-cli-sass'];
+
+    // Don't include the precompiled css file if the user uses a supported CSS preprocessor
+    if (!hasSass) {
+      app.import('vendor/ember-rdfa-editor.css');
+    }
+  },
 };
