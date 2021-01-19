e50e39db460be3f2e82871e6b671a2755f4a373a
Dieter Peirs <github@dieterpeirs.com>
2021-01-19 15:16:45 +0100

    fix index.js typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name;
+  name: require('./package').name,
 
   included() {
     let hasSass = !!app.registry.availablePlugins['ember-cli-sass'];
