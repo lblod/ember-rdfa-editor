e9a58d4b8a2263c046338796f90049d93b49a599
abeforgit <arnebertrand@gmail.com>
2021-03-01 18:37:48 +0100

    Refactor newline command

--- addon/commands/insert-newLine-command.ts
+++ addon/commands/insert-newLine-command.ts
@@ -3,6 +3,7 @@ import Model from "@lblod/ember-rdfa-editor/model/model";
 import ModelSelection from "@lblod/ember-rdfa-editor/model/model-selection";
 import ModelElement from "../model/model-element";
 import ModelNode from "../model/model-node";
+import {MisbehavedSelectionError, NoParentError} from "@lblod/ember-rdfa-editor/utils/errors";
 
 export default class InsertNewLineCommand extends Command {
   name = "insert-newLine";
@@ -17,83 +18,90 @@ export default class InsertNewLineCommand extends Command {
 
   execute(selection: ModelSelection = this.model.selection): void {
 
-    if (!selection.lastRange) {
-      throw new Error("no last range");
+    if (!ModelSelection.isWellBehaved(selection)) {
+      throw new MisbehavedSelectionError();
     }
 
-    const anchor = selection.lastRange.start.path;
-    const focus = selection.lastRange.end.path;
+    const selectionStart = selection.lastRange.start;
+    const selectionEnd = selection.lastRange.end;
+    const startPos = selectionStart.parentOffset;
+    const endPos = selectionEnd.parentOffset;
 
     const selectedIterator = selection.findAllInSelection({});
 
     if (!selectedIterator) {
-      throw new Error("couldn't get itterator");
+      throw new Error("couldn't get iterator");
     }
 
     const selected = Array.from(selectedIterator);
 
-    const firstText = selected.find(e => e.modelNodeType === 'TEXT');
-    const lastText = selected.reverse().find(e => e.modelNodeType === 'TEXT');
-    const anchorPos = anchor[anchor.length - 1];
-    const focusPos = focus[focus.length - 1];
+    // get the first and last textnodes of the selection
+    const firstText = selected.find(ModelNode.isModelText);
+    const lastText = selected.reverse().find(ModelNode.isModelText);
 
-    if (!firstText ||
-      !lastText ||
-      !ModelNode.isModelText(firstText) ||
-      !ModelNode.isModelText(lastText)) {
+    if (!firstText || !lastText) {
       throw new Error("couldn't get the first/last texts in selection");
     }
 
-    const firstSplit = firstText.split(anchorPos);
-    const lastSplit = lastText.split(focusPos);
-
-    if (!firstSplit || !lastSplit) {
-      throw new Error("couldn't split first/last text");
-    }
+    // split the node at the start of the selection
+    const {left: leftOfStart, right: rightOfStart} = firstText.split(startPos);
+    // split the node at the end of the selection
+    const {left: leftOfEnd, right: rightOfEnd} = lastText.split(endPos);
 
     const br = new ModelElement('br');
     let cursorPos = [0];
 
+    const leftParent = leftOfStart.parent;
+
+    if (!leftParent) {
+      throw new NoParentError();
+    }
+
     //handle zero length selection
-    if (anchor === focus) {
-      if (!firstSplit.left.parent) {
-        throw new Error("no parent, root element?");
-      }
-      firstSplit.left.parent.addChild(br, firstSplit.left.index! + 1);
-      cursorPos = [...split.right.getIndexPath()];
+    if (selection.isCollapsed) {
+      // add the break to the right of the node before the cursor
+      leftParent.addChild(br, leftOfStart.index! + 1);
+
+      // set the cursor to be at the start of the node to the right of the split
+      cursorPos = [...rightOfStart.getIndexPath()];
       cursorPos.push(0);
     }
     //handle long selection of single item
     else if (selected.length === 1) {
-      const split = firstText.split(anchorPos);
-      split.left.parent.addChild(br, split.left.index + 1);
-      const rightSplit = split.right.split(focusPos - split.left.length);
-      rightSplit.left.parent.removeChild(rightSplit.left);
-      cursorPos = [...rightSplit.right.getIndexPath()];
+      // add the break to the right of the node before the selection start
+      leftParent.addChild(br, leftOfStart.index! + 1);
+
+      // split the node at the end of the selection
+      const {left, right} = rightOfStart.split(endPos - leftOfStart.length);
+
+      // remove the middle part
+      this.model.removeModelNode(left);
+
+      // leave the cursor at the start of the right part of the split
+      cursorPos = [...right.getIndexPath()];
       cursorPos.push(0);
     }
     //handle multiple selected elems
     else {
-      //this should really be moved to a different function
-      const firstSplit = firstText.split(anchorPos);
-      firstSplit.right.parent.removeChild(firstSplit.right);
-      const lastSplit = lastText.split(focusPos);
-      lastSplit.left.parent.removeChild(lastSplit.left);
 
-      for (let i = 0; i < selected.length; i++) {
-        if (firstSplit.left.findAncestor(node => node === selected[i], false) ||
-          lastSplit.right.findAncestor(node => node === selected[i], false) ||
-          firstText === selected[i] ||
-          lastText === selected[i] ||
-          firstSplit.left === selected[i] ||
-          firstSplit.right === selected[i]) {
-          //do nothing
-        } else {
-          selected[i].parent?.removeChild(selected[i]);
+      //remove inner ends of selection
+      this.model.removeModelNode(rightOfStart);
+      this.model.removeModelNode(leftOfEnd);
+
+      // loop over selected elements and remove the ones that are not ancestors of the
+      // textnodes to the left and right of the selection
+
+      for (const selectedItem of selected) {
+        if (!(leftOfStart.findAncestor(node => node === selectedItem, true) ||
+          rightOfEnd.findAncestor(node => node === selectedItem, true) ||
+          firstText === selectedItem ||
+          lastText === selectedItem)) {
+          selectedItem.parent?.removeChild(selectedItem);
         }
       }
 
-      const cursorPos = [...lastSplit.right.getIndexPath()];
+      // leave the cursor at the start of the right part of the split
+      const cursorPos = [...rightOfEnd.getIndexPath()];
       cursorPos.push(0);
     }
 
