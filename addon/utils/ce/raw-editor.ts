55410133dee934c4c980e366a126b47aa7a4196a
Niels Vandekeybus <progster@gmail.com>
2021-04-13 15:53:04 +0200

    fix faulty merge

--- addon/utils/ce/raw-editor.ts
+++ addon/utils/ce/raw-editor.ts
@@ -1,7 +1,3 @@
-import classic from 'ember-classic-decorator';
-import RichNode from "@lblod/marawa/rich-node";
-import {walk as walkDomNode} from '@lblod/marawa/node-walker';
-import ModelElement from "@lblod/ember-rdfa-editor/model/model-element";
 import EmberObject from '@ember/object';
 import Command from "@lblod/ember-rdfa-editor/commands/command";
 import IndentListCommand from "@lblod/ember-rdfa-editor/commands/indent-list-command";
