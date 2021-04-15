3edf388e70e167fcc2845a7d44540448a63af380
Sergey Andreev <sergey.andreev@redpencil.io>
2021-04-15 16:50:57 +0200

    added tests for utility functions

--- tests/unit/model-selection-test.ts
+++ tests/unit/model-selection-test.ts
@@ -5,6 +5,9 @@ import ModelElement from "@lblod/ember-rdfa-editor/model/model-element";
 import ModelText from "@lblod/ember-rdfa-editor/model/model-text";
 import ModelNode from "@lblod/ember-rdfa-editor/model/model-node";
 import {AssertionError} from "@lblod/ember-rdfa-editor/utils/errors";
+import {vdom} from "@lblod/ember-rdfa-editor/model/util/xml-utils";
+import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
+import {INVISIBLE_SPACE} from "@lblod/ember-rdfa-editor/model/util/constants";
 
 module("Unit | model | model-selection", hooks => {
   const ctx = new ModelTestContext();
@@ -73,5 +76,91 @@ module("Unit | model | model-selection", hooks => {
     assert.strictEqual(result[3], li012);
 
   });
-});
+  test("collapsed selection inside an element", assert => {
+    // language=XML
+    const {root: initial, elements: {testLi}} = vdom`
+      <div>
+        <ul>
+          <li __id="testLi">
+            <text>${INVISIBLE_SPACE}</text>
+          </li>
+        </ul>
+      </div>
+    `;
 
+    ctx.model.rootModelNode.addChild(initial);
+    ctx.modelSelection.clearRanges();
+    ctx.modelSelection.selectRange(ModelRange.fromInElement(testLi, 0, 0));
+    const actual = ctx.model.rootModelNode.firstChild;
+
+    assert.true(ctx.modelSelection.isInside(["li"])==="enabled");
+  });
+  test("collapsed selection does not contain an element", assert => {
+    // language=XML
+    const {root: initial, elements: {testLi}} = vdom`
+      <div>
+        <ul>
+          <li __id="testLi">
+            <text>${INVISIBLE_SPACE}</text>
+          </li>
+        </ul>
+      </div>
+    `;
+
+    ctx.model.rootModelNode.addChild(initial);
+    ctx.modelSelection.clearRanges();
+    ctx.modelSelection.selectRange(ModelRange.fromInElement(testLi, 0, 0));
+    const actual = ctx.model.rootModelNode.firstChild;
+
+    assert.true(ctx.modelSelection.contains(["li"])==="disabled");
+  });
+  test("expanded selection not inside an element", assert => {
+    // language=XML
+    const {root: initial, textNodes: {testText}, elements: {testLi}} = vdom`
+      <div>
+        <text __id="testText">before li</text>
+        <ul>
+          <li __id="testLi">
+            <text>${INVISIBLE_SPACE}</text>
+          </li>
+        </ul>
+      </div>
+    `;
+
+    // debugger;
+    ctx.model.rootModelNode.addChild(initial);
+    const startPosition = ModelPosition.fromInTextNode(testText, 0);
+    const endPosition = ModelPosition.fromInElement(testLi, 0);
+    const range = new ModelRange(startPosition, endPosition);
+    ctx.modelSelection.clearRanges();
+    ctx.modelSelection.addRange(range);
+    const actual = ctx.model.rootModelNode.firstChild;
+
+    assert.true(ctx.modelSelection.isInside(["li"])==="disabled");
+  });
+  test("expanded selection contains an element", assert => {
+    // language=XML
+    const {root: initial, textNodes: {testText}, elements: {testLi}} = vdom`
+      <div>
+        <text __id="testText">before li</text>
+        <ul>
+          <li __id="testLi">
+            <text>${INVISIBLE_SPACE}</text>
+          </li>
+        </ul>
+      </div>
+    `;
+
+    // debugger;
+    ctx.model.rootModelNode.addChild(initial);
+    const startPosition = ModelPosition.fromInTextNode(testText, 0);
+    const endPosition = ModelPosition.fromInElement(testLi, 0);
+    const range = new ModelRange(startPosition, endPosition);
+    ctx.modelSelection.clearRanges();
+    ctx.modelSelection.addRange(range);
+    const actual = ctx.model.rootModelNode.firstChild;
+
+    assert.true(ctx.modelSelection.contains(["li"])==="enabled");
+  });
+
+});
