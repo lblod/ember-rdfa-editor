35fad6ab56fb5edcf9b415c0fd460b3ec7fde8a2
abeforgit <arnebertrand@gmail.com>
2021-04-22 22:33:29 +0200

    Implement split operation

--- addon/model/operations/operation-algorithms.ts
+++ addon/model/operations/operation-algorithms.ts
@@ -1,6 +1,7 @@
 import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
 import ModelNode from "@lblod/ember-rdfa-editor/model/model-node";
 import ModelTreeWalker from "@lblod/ember-rdfa-editor/model/util/model-tree-walker";
+import ModelPosition from "@lblod/ember-rdfa-editor/model/model-position";
 
 /**
  * A shared library of algorithms to be used by operations only
@@ -12,7 +13,7 @@ export default class OperationAlgorithms {
     const confinedRanges = range.getMinimumConfinedRanges();
     const nodesToRemove = [];
     for (const range of confinedRanges) {
-      if(!range.collapsed) {
+      if (!range.collapsed) {
         const walker = new ModelTreeWalker({range, descend: false});
         nodesToRemove.push(...walker);
       }
@@ -45,10 +46,37 @@ export default class OperationAlgorithms {
     rangeToMove.start.split();
     rangeToMove.end.split();
     const nodes = OperationAlgorithms.remove(rangeToMove);
-    if(nodes.length) {
+    if (nodes.length) {
       OperationAlgorithms.insert(targetRange, ...nodes);
     }
     return nodes;
+  }
+
+  static splitText(position: ModelPosition) {
+    position.split();
+    return position;
+  }
+
+  static split(position: ModelPosition): ModelPosition {
+    OperationAlgorithms.splitText(position);
+    const parent = position.parent;
+    if (parent === position.root) {
+      return position;
+    }
+    const grandParent = parent.parent;
+    if (!grandParent) {
+      return position;
+    }
+
+    const newNode = parent.shallowClone();
+    const after = position.nodeAfter();
+    if (after) {
+      const rightSideChildren = parent.children.splice(after.index!);
+      rightSideChildren[0].previousSibling = null;
+      newNode.appendChildren(...rightSideChildren);
+    }
+    grandParent.addChild(newNode, parent.index! + 1);
+    return ModelPosition.fromBeforeNode(newNode);
   }
 
 }
---
+++ addon/model/operations/split-operation.ts
@@ -0,0 +1,48 @@
+import Operation from "@lblod/ember-rdfa-editor/model/operations/operation";
+import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
+import OperationAlgorithms from "@lblod/ember-rdfa-editor/model/operations/operation-algorithms";
+import ModelPosition from "@lblod/ember-rdfa-editor/model/model-position";
+import {ModelError, OperationError} from "@lblod/ember-rdfa-editor/utils/errors";
+
+export default class SplitOperation extends Operation {
+  private _splitParent: boolean;
+
+  constructor(range: ModelRange, splitParent = true) {
+    super(range);
+    this._splitParent = splitParent;
+  }
+
+  get splitParent(): boolean {
+    return this._splitParent;
+  }
+
+  set splitParent(value: boolean) {
+    this._splitParent = value;
+  }
+
+  execute(): ModelRange {
+    if (this.range.collapsed) {
+      const newPos = this.doSplit(this.range.start);
+      return new ModelRange(newPos, newPos);
+    } else {
+      // this is very fragile and depends heavily on execution order.
+      // be careful making changes here
+      const end = this.doSplit(this.range.end);
+      const afterEnd = end.nodeAfter();
+      if(!afterEnd) {
+        throw new ModelError("Unexpected model state");
+      }
+      const start = this.doSplit(this.range.start);
+      return new ModelRange(start, ModelPosition.fromBeforeNode(afterEnd));
+    }
+  }
+
+  private doSplit(position: ModelPosition) {
+    if (this._splitParent) {
+      return OperationAlgorithms.split(position);
+    } else {
+      return OperationAlgorithms.splitText(position);
+    }
+  }
+
+}
