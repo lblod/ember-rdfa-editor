75bb26013aa9236a26cc7c4b94c44e1ab2ea648f
abeforgit <arnebertrand@gmail.com>
2021-05-06 19:21:38 +0200

    Implement placeholder plugin logic

--- addon/utils/plugins/anchor-tags/text-input-plugin.ts
+++ addon/utils/plugins/anchor-tags/text-input-plugin.ts
@@ -15,24 +15,32 @@ export default class AnchorTagTextInputPlugin implements TextInputPlugin {
       const clonedRange = range.clone();
       const collapsed = clonedRange.collapsed;
       const {start, end, start: {parent: startParent}, end: {parent: endParent}} = clonedRange;
+      let anyAnchors = false;
 
       if (startParent.type === "a" && start.parentOffset === 0) {
+        anyAnchors = true;
         clonedRange.start = ModelPosition.fromBeforeNode(startParent);
         if (collapsed) {
           clonedRange.collapse(true);
         }
       }
       if (endParent.type === "a" && end.parentOffset === endParent.getMaxOffset()) {
+        anyAnchors = true;
         clonedRange.end = ModelPosition.fromAfterNode(endParent);
         if (collapsed) {
           clonedRange.collapse();
         }
       }
-      return {
-        allow: true, executor: (_, rawEditor: PernetRawEditor) => {
-          rawEditor.executeCommand("insert-text", text, clonedRange);
-        }
-      };
+      if(anyAnchors) {
+        return {
+          allow: true, executor: (_, rawEditor: PernetRawEditor) => {
+            rawEditor.executeCommand("insert-text", text, clonedRange);
+          }
+        };
+
+      } else {
+        return null;
+      }
     }
     return null;
   }
