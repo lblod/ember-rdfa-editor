73d8bca7a21cdfec4556a3ff51491d25b6b01721
abeforgit <arnebertrand@gmail.com>
2021-05-11 16:20:45 +0200

    Replace space literals with the space constant

--- addon/commands/insert-text-command.ts
+++ addon/commands/insert-text-command.ts
@@ -2,7 +2,7 @@ import Command from "@lblod/ember-rdfa-editor/commands/command";
 import Model from "@lblod/ember-rdfa-editor/model/model";
 import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
 import {MisbehavedSelectionError} from "@lblod/ember-rdfa-editor/utils/errors";
-import {NON_BREAKING_SPACE} from "@lblod/ember-rdfa-editor/model/util/constants";
+import {NON_BREAKING_SPACE, SPACE} from "@lblod/ember-rdfa-editor/model/util/constants";
 
 export default class InsertTextCommand extends Command {
   name = "insert-text";
@@ -16,9 +16,9 @@ export default class InsertTextCommand extends Command {
       throw new MisbehavedSelectionError();
     }
     this.model.change(mutator => {
-      if (text.charAt(0) === " ") {
+      if (text.charAt(0) === SPACE) {
         const charBefore = range.start.charactersBefore(1);
-        if (charBefore === " ") {
+        if (charBefore === SPACE) {
           text = NON_BREAKING_SPACE + text;
           range.start = range.start.shiftedBy(-1);
         }
