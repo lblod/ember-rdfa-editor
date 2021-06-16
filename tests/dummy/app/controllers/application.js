50e1803ca3c3d2d6dd994fe6fa0b7b5b5aa6ca67
Niels Vandekeybus <progster@gmail.com>
2021-06-16 10:31:11 +0200

    make it easier to configure paste behaviour in test app

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,11 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-
+import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
   @tracked rdfaEditor;
   @tracked debug;
+  @service() features;
 
   @action
   initDebug(info) {
@@ -21,5 +22,22 @@ export default class ApplicationController extends Controller {
   @action
   loadData(data) {
     this.rdfaEditor.setHtmlContent(data);
+  }
+
+  @action
+  setPasteBehaviour(event) {
+    const val = event.target.value;
+    if (val === "textonly") {
+      this.features.disable('editor-extended-html-paste');
+      this.features.disable('editor-html-paste');
+    }
+    else if (val === "limited") {
+      this.features.disable('editor-extended-html-paste');
+      this.features.enable('editor-html-paste');
+    }
+    else if (val === "full") {
+      this.features.enable('editor-extended-html-paste');
+      this.features.enable('editor-html-paste');
+    }
   }
 }
