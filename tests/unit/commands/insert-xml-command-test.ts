26946ae15209f85bb600466117798edfb2a5aa58
Robbe De Proft <rdeproft12@gmail.com>
2021-07-05 14:21:47 +0200

    Create simple insert XML command test

---
+++ tests/unit/commands/insert-xml-command-test.ts
@@ -0,0 +1,41 @@
+import {module, test} from "qunit";
+import ModelTestContext from "dummy/tests/utilities/model-test-context";
+import InsertXmlCommand from "@lblod/ember-rdfa-editor/commands/insert-xml-command";
+import {vdom} from "@lblod/ember-rdfa-editor/model/util/xml-utils";
+import {oneLineTrim} from "common-tags";
+import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
+
+module("Unit | commands | insert-xml-command-test", hooks => {
+  const ctx = new ModelTestContext();
+  let command: InsertXmlCommand;
+
+  hooks.beforeEach(() => {
+    ctx.reset();
+    command = new InsertXmlCommand(ctx.model);
+  });
+
+  test("inserts correctly in empty document", assert => {
+    // language=XML
+    const {root: initial} = vdom`
+      <modelRoot/>
+    `;
+
+    // language=XML
+    const {root: expected} = vdom`
+      <modelRoot>
+        <div>
+          <text>hello world</text>
+        </div>
+      </modelRoot>
+    `;
+
+    const xmlToInsert = oneLineTrim`<div><text>hello world</text><div/>`;
+
+    ctx.model.fillRoot(initial);
+    const range = ModelRange.fromInElement(ctx.model.rootModelNode, 0, 0);
+    ctx.model.selectRange(range);
+
+    command.execute(xmlToInsert);
+    assert.true(ctx.model.rootModelNode.sameAs(expected));
+  });
+});
