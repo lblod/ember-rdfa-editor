2848c08b4f3e1137db0e512ba6efeebe54e9485e
Robbe De Proft <rdeproft12@gmail.com>
2021-07-05 16:20:46 +0200

    Add range tests for insert xml command

--- tests/unit/commands/insert-xml-command-test.ts
+++ tests/unit/commands/insert-xml-command-test.ts
@@ -4,6 +4,7 @@ import InsertXmlCommand from "@lblod/ember-rdfa-editor/commands/insert-xml-comma
 import {vdom} from "@lblod/ember-rdfa-editor/model/util/xml-utils";
 import {oneLineTrim} from "common-tags";
 import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
+import ModelPosition from "@lblod/ember-rdfa-editor/model/model-position";
 
 module("Unit | commands | insert-xml-command-test", hooks => {
   const ctx = new ModelTestContext();
@@ -61,6 +62,112 @@ module("Unit | commands | insert-xml-command-test", hooks => {
 
     ctx.model.fillRoot(initial);
     const range = ModelRange.fromInElement(ctx.model.rootModelNode, 0, 0);
+    ctx.model.selectRange(range);
+
+    command.execute(xmlToInsert);
+    assert.true(ctx.model.rootModelNode.sameAs(expected));
+  });
+
+  test("inserts correctly inside text node", assert => {
+    // language=XML
+    const {root: initial} = vdom`
+      <modelRoot>
+        <text>elephant</text>
+      </modelRoot>
+    `;
+
+    // language=XML
+    const {root: expected} = vdom`
+      <modelRoot>
+        <text>ele</text>
+        <div>
+          <text>hello world</text>
+        </div>
+        <text>phant</text>
+      </modelRoot>
+    `;
+
+    const xmlToInsert = oneLineTrim`<div><text>hello world</text></div>`;
+
+    ctx.model.fillRoot(initial);
+    const range = ModelRange.fromInElement(ctx.model.rootModelNode, 3, 3);
+    ctx.model.selectRange(range);
+
+    command.execute(xmlToInsert);
+    assert.true(ctx.model.rootModelNode.sameAs(expected));
+  });
+
+  test("correctly replaces part of text node", assert => {
+    // language=XML
+    const {root: initial} = vdom`
+      <modelRoot>
+        <text>elephant</text>
+      </modelRoot>
+    `;
+
+    // language=XML
+    const {root: expected} = vdom`
+      <modelRoot>
+        <text>el</text>
+        <div>
+          <text>hello world</text>
+        </div>
+        <text>ant</text>
+      </modelRoot>
+    `;
+
+    const xmlToInsert = oneLineTrim`<div><text>hello world</text></div>`;
+
+    ctx.model.fillRoot(initial);
+    const range = ModelRange.fromInElement(ctx.model.rootModelNode, 2, 5);
+    ctx.model.selectRange(range);
+
+    command.execute(xmlToInsert);
+    assert.true(ctx.model.rootModelNode.sameAs(expected));
+  });
+
+  test("correctly replaces complex range", assert => {
+    // language=XML
+    const {root: initial, textNodes: {rangeStart, rangeEnd}} = vdom`
+      <modelRoot>
+        <div>
+          <text __id="rangeStart">elephant</text>
+          <span>
+            <span/>
+            <span>
+              <text __id="rangeEnd">monkey</text>
+            </span>
+            <span/>
+          </span>
+        </div>
+      </modelRoot>
+    `;
+
+    // language=XML
+    const {root: expected} = vdom`
+      <modelRoot>
+        <div>
+          <text>ele</text>
+          <div>
+            <text>hello world</text>
+          </div>
+          <span>
+            <span>
+              <text>key</text>
+            </span>
+            <span/>
+          </span>
+        </div>
+      </modelRoot>
+    `;
+
+    const xmlToInsert = oneLineTrim`<div><text>hello world</text></div>`;
+
+    ctx.model.fillRoot(initial);
+    const range = new ModelRange(
+      ModelPosition.fromInTextNode(rangeStart, 3),
+      ModelPosition.fromInTextNode(rangeEnd, 3)
+    );
     ctx.model.selectRange(range);
 
     command.execute(xmlToInsert);
