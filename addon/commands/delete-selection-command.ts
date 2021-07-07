7d399fa9a4bb2c146dbcead2ed3c2870b1103f1e
Robbe De Proft <rdeproft12@gmail.com>
2021-07-07 16:06:30 +0200

    Implement delete selection command

---
+++ addon/commands/delete-selection-command.ts
@@ -0,0 +1,46 @@
+import Command from "@lblod/ember-rdfa-editor/commands/command";
+import Model from "@lblod/ember-rdfa-editor/model/model";
+import ModelSelection from "@lblod/ember-rdfa-editor/model/model-selection";
+import {MisbehavedSelectionError} from "@lblod/ember-rdfa-editor/utils/errors";
+import ModelTreeWalker from "@lblod/ember-rdfa-editor/model/util/model-tree-walker";
+import ModelNode from "@lblod/ember-rdfa-editor/model/model-node";
+import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
+
+export default class DeleteSelectionCommand extends Command<ModelNode[]> {
+  name = "delete-selection-command";
+
+  constructor(model: Model) {
+    super(model);
+  }
+
+  execute(selection: ModelSelection = this.model.selection): ModelNode[] {
+    if (!ModelSelection.isWellBehaved(selection)) {
+      throw new MisbehavedSelectionError();
+    }
+
+    let modelNodes: ModelNode[] = [];
+    const commonAncestor = selection.getCommonAncestor().parent;
+
+    this.model.change(mutator => {
+      let contentRange = mutator.splitRangeUntilElements(selection.lastRange, commonAncestor, commonAncestor);
+      const treeWalker = new ModelTreeWalker({
+        range: contentRange,
+        descend: false
+      });
+
+      // Check if selection is inside table cell. If this is the case, cut children of said cell.
+      // Assumption: if table cell is selected, no other nodes at the same level can be selected.
+      const firstModelNode = treeWalker.currentNode;
+      if (ModelNode.isModelElement(firstModelNode) && firstModelNode.type === "td") {
+        contentRange = ModelRange.fromInNode(firstModelNode, 0, firstModelNode.getMaxOffset());
+        modelNodes = firstModelNode.children;
+      } else {
+        modelNodes = [...treeWalker];
+      }
+
+      selection.selectRange(mutator.insertNodes(contentRange));
+    });
+
+    return modelNodes;
+  }
+}
