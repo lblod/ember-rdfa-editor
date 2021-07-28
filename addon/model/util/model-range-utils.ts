76c37480234ddea33fa9468c12dd39a16a427ccb
Robbe De Proft <rdeproft12@gmail.com>
2021-07-28 18:16:51 +0200

    Fix linting errors

--- addon/model/util/model-range-utils.ts
+++ addon/model/util/model-range-utils.ts
@@ -1,7 +1,6 @@
 import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
 import ModelPosition from "@lblod/ember-rdfa-editor/model/model-position";
 import ModelNodeUtils from "@lblod/ember-rdfa-editor/model/util/model-node-utils";
-import {ElementType} from "@lblod/ember-rdfa-editor/model/model-element";
 import ModelTreeWalker, {toFilterSkipFalse} from "@lblod/ember-rdfa-editor/model/util/model-tree-walker";
 import ModelNode from "@lblod/ember-rdfa-editor/model/model-node";
 
