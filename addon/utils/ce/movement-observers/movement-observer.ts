8b881ec2a04b7127c807c74cb17b33e2736188bb
Niels Vandekeybus <progster@gmail.com>
2021-07-28 21:03:15 +0200

    fix type imports

--- addon/utils/ce/movement-observers/movement-observer.ts
+++ addon/utils/ce/movement-observers/movement-observer.ts
@@ -1,5 +1,5 @@
-import {InternalSelection} from "@lblod/ember-rdfa-editor/editor/raw-editor";
 import RawEditor from "@lblod/ember-rdfa-editor/utils/ce/raw-editor";
+import { InternalSelection } from "../pernet-raw-editor";
 
 export default interface MovementObserver {
   handleMovement: (document: RawEditor, oldSelection?: InternalSelection, newSelection?: InternalSelection) => void;
