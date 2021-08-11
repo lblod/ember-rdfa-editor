5464015a90a66346ea6b53607815c98356cd761d
Robbe De Proft <rdeproft12@gmail.com>
2021-08-11 16:25:55 +0200

    Sensible behaviour delete li forwards

--- addon/commands/delete-li-forwards-command.ts
+++ addon/commands/delete-li-forwards-command.ts
@@ -3,6 +3,9 @@ import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
 import ModelNodeUtils from "@lblod/ember-rdfa-editor/model/util/model-node-utils";
 import {MisbehavedSelectionError} from "@lblod/ember-rdfa-editor/utils/errors";
 import Model from "@lblod/ember-rdfa-editor/model/model";
+import ModelPosition from "@lblod/ember-rdfa-editor/model/model-position";
+import ModelRangeUtils from "@lblod/ember-rdfa-editor/model/util/model-range-utils";
+import ModelTreeWalker from "@lblod/ember-rdfa-editor/model/util/model-tree-walker";
 
 export default class DeleteLiForwardsCommand extends Command {
   name = "delete-li-forwards";
@@ -26,5 +29,63 @@ export default class DeleteLiForwardsCommand extends Command {
     if (!range) {
       throw new MisbehavedSelectionError();
     }
+
+    // Find the uppermost list container of the list containing the list element.
+    const listAncestors = range.start.findAncestors(ModelNodeUtils.isListContainer);
+    const topListContainer = listAncestors[listAncestors.length - 1];
+
+    // Search the next list element starting from after the current list element and ending right
+    // behind the uppermost list container.
+    const searchRange = new ModelRange(
+      ModelPosition.fromAfterNode(range.start.parent),
+      ModelPosition.fromInNode(topListContainer, topListContainer.getMaxOffset())
+    );
+    const nextListElement = ModelRangeUtils.findFirstListElement(searchRange);
+
+    this.model.change(mutator => {
+      if (nextListElement) {
+        const moveRange = ModelRange.fromInElement(nextListElement, 0, nextListElement?.getMaxOffset());
+        const nodesToMove = DeleteLiForwardsCommand.getNodesInRange(moveRange);
+
+        mutator.insertNodes(ModelRange.fromAroundNode(nextListElement));
+        mutator.insertNodes(range, ...nodesToMove);
+      } else {
+        const positionAfterList = ModelPosition.fromAfterNode(topListContainer);
+        const nodeAfterList = positionAfterList.nodeAfter();
+
+        if (nodeAfterList
+          && !ModelNodeUtils.isListContainer(nodeAfterList)
+          && !ModelNodeUtils.isTableContainer(nodeAfterList)
+        ) {
+          let rangeAround: ModelRange;
+          if (ModelNodeUtils.isBr(nodeAfterList.nextSibling)) {
+            rangeAround = new ModelRange(
+              ModelPosition.fromBeforeNode(nodeAfterList),
+              ModelPosition.fromAfterNode(nodeAfterList.nextSibling)
+            );
+          } else {
+            rangeAround = ModelRange.fromAroundNode(nodeAfterList);
+          }
+
+          mutator.insertNodes(rangeAround);
+          if (!ModelNodeUtils.isBr(nodeAfterList)) {
+            mutator.insertNodes(range, nodeAfterList);
+          }
+        }
+      }
+    });
+  }
+
+  private static getNodesInRange(range: ModelRange) {
+    if (!range.collapsed) {
+      const treeWalker = new ModelTreeWalker({
+        range: range,
+        descend: false
+      });
+
+      return [...treeWalker];
+    }
+
+    return [];
   }
 }
--- addon/commands/delete-list-forwards-command.ts
+++ addon/commands/delete-list-forwards-command.ts
@@ -1,8 +1,12 @@
 import Command from "@lblod/ember-rdfa-editor/commands/command";
 import ModelRange from "@lblod/ember-rdfa-editor/model/model-range";
 import ModelNodeUtils from "@lblod/ember-rdfa-editor/model/util/model-node-utils";
-import {MisbehavedSelectionError} from "@lblod/ember-rdfa-editor/utils/errors";
+import {IllegalExecutionStateError, MisbehavedSelectionError, ModelError} from "@lblod/ember-rdfa-editor/utils/errors";
 import Model from "@lblod/ember-rdfa-editor/model/model";
+import ModelRangeUtils from "@lblod/ember-rdfa-editor/model/util/model-range-utils";
+import ModelPosition from "@lblod/ember-rdfa-editor/model/model-position";
+import ModelNode from "@lblod/ember-rdfa-editor/model/model-node";
+import ModelTreeWalker from "@lblod/ember-rdfa-editor/model/util/model-tree-walker";
 
 export default class DeleteListForwardsCommand extends Command {
   name = "delete-list-forwards";
@@ -23,5 +27,64 @@ export default class DeleteListForwardsCommand extends Command {
     if (!range) {
       throw new MisbehavedSelectionError();
     }
+
+    const nodeAfter = range.start.nodeAfter();
+    if (!nodeAfter) {
+      throw new IllegalExecutionStateError("No node after the cursor");
+    }
+
+    const listRange = ModelRange.fromAroundNode(nodeAfter);
+    const firstListElement = ModelRangeUtils.findFirstListElement(listRange);
+    if (!ModelNodeUtils.isListElement(firstListElement)) {
+      throw new ModelError("List without any list elements");
+    }
+
+    const firstListContainer = firstListElement.findFirstChild(ModelNodeUtils.isListContainer);
+    const rangeAroundLi = ModelRange.fromAroundNode(firstListElement);
+
+    let liFullySelected: boolean;
+    let rangeInsideLi: ModelRange;
+    if (firstListContainer) {
+      liFullySelected = false;
+      rangeInsideLi = new ModelRange(
+        ModelPosition.fromInNode(firstListElement, 0),
+        ModelPosition.fromBeforeNode(firstListContainer)
+      );
+    } else {
+      liFullySelected = true;
+      rangeInsideLi = new ModelRange(
+        ModelPosition.fromInNode(firstListElement, 0),
+        ModelPosition.fromInNode(firstListElement, firstListElement.getMaxOffset()),
+      );
+    }
+
+    let nodesToMove: ModelNode[] = [];
+    if (rangeInsideLi.start.parentOffset !== rangeInsideLi.end.parentOffset) {
+      const treeWalker = new ModelTreeWalker({
+        range: rangeInsideLi,
+        descend: false
+      });
+      nodesToMove = [...treeWalker];
+    }
+
+    const bottomListContainer = ModelNodeUtils.findAncestor(firstListElement, ModelNodeUtils.isListContainer);
+    if (!bottomListContainer) {
+      throw new ModelError("List element without list container");
+    }
+
+    this.model.change(mutator => {
+      if (liFullySelected
+        && rangeAroundLi.start.nodeBefore() === null
+        && rangeAroundLi.end.nodeAfter() === null
+      ) {
+        mutator.insertNodes(ModelRange.fromAroundNode(bottomListContainer));
+      } else if (liFullySelected) {
+        mutator.insertNodes(rangeAroundLi);
+      } else {
+        mutator.insertNodes(rangeInsideLi);
+      }
+
+      mutator.insertNodes(range, ...nodesToMove);
+    });
   }
 }
