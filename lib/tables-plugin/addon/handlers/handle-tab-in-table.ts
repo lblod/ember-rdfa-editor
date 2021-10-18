857b796e3e5f6b5ec5c751d30b7e186ea98fc3eb
Oscar <lagartoverde97@gmail.com>
2021-10-18 11:56:48 +0200

    fixed check for in table state

--- lib/tables-plugin/addon/handlers/handle-tab-in-table.ts
+++ lib/tables-plugin/addon/handlers/handle-tab-in-table.ts
@@ -1,10 +1,11 @@
 import ModelTable from "@lblod/ember-rdfa-editor/core/model/model-table";
 import ModelElement from "@lblod/ember-rdfa-editor/core/model/model-element";
 import EditorController from "@lblod/ember-rdfa-editor/core/editor-controller";
+import { PropertyState } from "@lblod/ember-rdfa-editor/util/types";
 
 export default function HandleTabInTable(reverse: boolean, controller: EditorController) {
   const selection = controller.selection;
-  if(selection.inTableState) {
+  if(selection.inTableState !== PropertyState.disabled) {
     let cell = ModelTable.getCellFromSelection(selection);
 
     if(!cell){
