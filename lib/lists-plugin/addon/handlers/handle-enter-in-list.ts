9d1870f41cada4fd30d32273cec84952f562ead2
Oscar <lagartoverde97@gmail.com>
2021-10-19 14:37:31 +0200

    fix naming errors

--- lib/lists-plugin/addon/handlers/handle-enter-in-list.ts
+++ lib/lists-plugin/addon/handlers/handle-enter-in-list.ts
@@ -3,7 +3,7 @@ import ModelElement from "@lblod/ember-rdfa-editor/core/model/model-element";
 import EditorController from "@lblod/ember-rdfa-editor/core/editor-controller";
 import { PropertyState } from "@lblod/ember-rdfa-editor/util/types";
  
-export default function HandleTabInList(controller: EditorController) {
+export default function handleEnterInList(controller: EditorController) {
   const selection = controller.selection;
   if(selection.inListState === PropertyState.enabled) {
     controller.executeCommand('insert-newLi');
--- lib/lists-plugin/addon/handlers/handle-tab-in-list.ts
+++ lib/lists-plugin/addon/handlers/handle-tab-in-list.ts
@@ -3,7 +3,7 @@ import ModelElement from "@lblod/ember-rdfa-editor/core/model/model-element";
 import EditorController from "@lblod/ember-rdfa-editor/core/editor-controller";
 import { PropertyState } from "@lblod/ember-rdfa-editor/util/types";
  
-export default function HandleTabInList(reverse: boolean, controller: EditorController) {
+export default function handleTabInList(reverse: boolean, controller: EditorController) {
   const selection = controller.selection;
   if(selection.inListState === PropertyState.enabled) {
     const startPositionOffset = selection.getRangeAt(0).start.parentOffset;
