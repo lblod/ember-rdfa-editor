e325a99453cc2867bb2d28b1506faecb213c2d05
Niels Vandekeybus <progster@gmail.com>
2021-10-27 15:19:10 +0200

    improve event handling
    currently hooking into beforeinput, paste, cut and copy events

--- addon/components/ce/content-editable.ts
+++ addon/components/ce/content-editable.ts
@@ -3,7 +3,9 @@ import {inject as service} from '@ember/service';
 import Component from '@glimmer/component';
 import Editor, {EditorImpl} from "@lblod/ember-rdfa-editor/core/editor";
 import {UninitializedError} from "@lblod/ember-rdfa-editor/util/errors";
-import {CopyEvent, CutEvent, KeydownEvent, PasteEvent} from "@lblod/ember-rdfa-editor/core/editor-events";
+import { CopyEvent, CutEvent, PasteEvent} from "@lblod/ember-rdfa-editor/core/editor-events";
+import { DeleteContentForwardEvent, DeleteContentBackwardEvent} from "@lblod/ember-rdfa-editor/core/editor-events";
+import { InsertLineBreakEvent, InsertParagraphEvent, InsertTextEvent } from "@lblod/ember-rdfa-editor/core/editor-events";
 
 interface FeatureService {
   isEnabled(key: string): boolean
@@ -34,33 +36,6 @@ export default class ContentEditable extends Component<ContentEditableArgs> {
   /**
    * @constructor
    */
-  constructor(owner: unknown, args: ContentEditableArgs) {
-    super(owner, args);
-    document.addEventListener("selectionchange", this.handleSelectionChange);
-    // const rawEditor = PernetRawEditor.create({});
-    // rawEditor.registerMovementObserver(new LumpNodeMovementObserver());
-    //
-    // this._rawEditor = rawEditor;
-    // this.defaultHandlers = [
-    //   new ArrowHandler({ rawEditor }),
-    //   new EnterHandler({ rawEditor }),
-    //   new BackspaceHandler({ rawEditor }),
-    //   new TabHandler({ rawEditor }),
-    //   new TextInputHandler({ rawEditor }),
-    //   new DisableDeleteHandler({ rawEditor }),
-    //   new IgnoreModifiersHandler({ rawEditor }),
-    //   new UndoHandler({ rawEditor }),
-    //   new BoldItalicUnderlineHandler({ rawEditor }),
-    //   new EscapeHandler({ rawEditor }),
-    //   new FallbackInputHandler({ rawEditor }),
-    // ];
-    //
-    // this.externalHandlers = this.args.externalHandlers ? this.args.externalHandlers : [];
-    // this.cutHandler = new CutHandler({rawEditor});
-    // this.copyHandler = new CopyHandler({rawEditor});
-    // this.pasteHandler = new PasteHandler({rawEditor});
-  }
-
   get editor(): Editor {
     if (!this._editor) {
       throw new UninitializedError("Tried to access editor before contenteditable component was inserted");
@@ -80,29 +55,37 @@ export default class ContentEditable extends Component<ContentEditableArgs> {
     await this.args.editorInit(editor);
   }
 
-  /**
-   * Tries to find and identify common keyboard shortcuts which emit other events we can tap into.
-   * Currently tries to catch copy, paste, cut. Definitely needs testing on mac.
-   * @method keydownMapsToOtherEvent
-   */
-  keydownMapsToOtherEvent(event: KeyboardEvent): boolean {
-    return (event.ctrlKey || event.metaKey) && ["v", "c", "x"].includes(event.key);
-  }
-
-  excludedKeys = new Set(["F12", "ArrowDown", "ArrowUp", "CapsLock", "Home", "PageUp", "PageDown", "End"]);
-
   @action
-  handleKeyDown(event: KeyboardEvent) {
-    if (!this.keydownMapsToOtherEvent(event) && !this.excludedKeys.has(event.key)) {
-      console.log(event.key);
+  handleInput(event: InputEvent) {
+    // https://rawgit.com/w3c/input-events/v1/index.html#interface-InputEvent-Attributes
+    if (event.inputType == "insertText") {
       event.preventDefault();
-      this.editor.emitEvent(new KeydownEvent(event, CE_OWNER));
+      this.editor.emitEvent(new InsertTextEvent(event, CE_OWNER))
+    }
+    else if (event.inputType == "deleteContentBackward") {
+      event.preventDefault();
+      this.editor.emitEvent(new DeleteContentBackwardEvent(event, CE_OWNER));
+    }
+    else if (event.inputType == "deleteContentForward") {
+      event.preventDefault();
+      this.editor.emitEvent(new DeleteContentForwardEvent(event, CE_OWNER));
+    }
+    else if (event.inputType == "insertParagraph") {
+      event.preventDefault();
+      this.editor.emitEvent(new InsertParagraphEvent(event, CE_OWNER));
+    }
+    else if (event.inputType == "insertLineBreak") {
+      event.preventDefault();
+      this.editor.emitEvent(new InsertLineBreakEvent(event, CE_OWNER));
+    }
+    else {
+      console.log(`did not prevent beforeinput event of type ${event.inputType}`,event);
     }
   }
 
   @action
-  handleSelectionChange() {
-    // this.editor.emitEvent(new SelectionChangedEvent(CE_OWNER));
+  recoverFromUnhandledInput(event: InputEvent) {
+    console.log(event);
   }
 
   willDestroy() {
@@ -144,196 +127,4 @@ export default class ContentEditable extends Component<ContentEditableArgs> {
       }));
     }
   }
-
-  //
-  // @action
-  // handleMouseUp(event: MouseEvent) {
-  //   const preventDefault = this.passEventToHandlers(event);
-  // cutHandler: InputHandler;
-  // copyHandler: InputHandler;
-  // pasteHandler: InputHandler;
-  //
-  // _rawEditor: PernetRawEditor;
-  //
-  // /**
-  //  * Element of the component. It is aliased to the rawEditor.rootElement.
-  //  *
-  //  * @property element
-  //  * @type HTMLElement
-  //  *
-  //  * @private
-  //  */
-  // @tracked rootElement: HTMLElement | null = null;
-  //
-  // /**
-  //  *
-  //  * @property rawEditor
-  //  * @type RawEditor
-  //  */
-  // get rawEditor(): PernetRawEditor {
-  //   if (!this._rawEditor) {
-  //     throw new IllegalAccessToRawEditor();
-  //   }
-  //   return this._rawEditor;
-  // }
-  //
-  // /**
-  //  * Ordered set of input handlers.
-  //  * @property eventHandlers
-  //  * @type Array
-  //  * @public
-  //  */
-  // get inputHandlers(): InputHandler[] {
-  //   return this.externalHandlers.concat(this.defaultHandlers);
-  // }
-  //
-  // /**
-  //  * Default input handlers.
-  //  * @property defaultHandlers
-  //  * @type Array
-  //  * @private
-  //  */
-  // @tracked defaultHandlers: InputHandler[];
-  //
-  // /**
-  //  * External input handlers.
-  //  * @property externalHandlers
-  //  * @type Array
-  //  * @private
-  //  */
-  // externalHandlers: InputHandler[];
-  //
-  //
-  // /**
-  //  * the following block handles input, as it's taken some research I'll dump some knowledge and assumptions here
-  //  * take this with a grain of salt, because the spec is somewhat unclear and not all browser implement the same order:
-  //  *
-  //  * Chain 1: (regular input)
-  //  * keydown (captured if you want to prevent the key from doing something do it here, preventdefault will prevent keypress and input from firing)
-  //  * keypress (we ignore this, it's badly spec'ed and behaves differently in different browsers)
-  //  * beforeinput (sometimes captured, has potential, because it can replace the entire keydown/keypress mess. currently only in chrome)
-  //  * keyup (we should ignore this, because it fires even if keydown does a preventDefault. however it is one of the few places we can capture page up, page down, arrow up and down so we capture those here using the fallback input handler)
-  //  * input (captured, input has happened and all you can do is clean up)
-  //  *
-  //  * Chain 2 (in FF input fires after compositionend): compositions (mostly on mac)
-  //  * compositionupdate (we ignore this)
-  //  * input (we capture this)
-  //  * compositionend (we capture this, if input has preventdefault this doesn't fire. not well tested)
-  //  *
-  //  * Chain 3: copy, cut, paste (may be broken if event order changes)
-  //  * keydown (captured, we try to ignore the event here and let it bubble)
-  //  * keypress (we ignore this, see above)
-  //  * keyup (we ignore this, see above)
-  //  * paste/copy/cut/undo (we capture these)
-  //  * input (prevent default prevents this one from triggering)
-  //  */
-  //
-  // /**
-  //  * keyDown events are handled for simple input we take over from
-  //  * browser input.
-  //  */
-  // @action
-  // handleKeyDown(event: KeyboardEvent) {
-  //   if (!this.keydownMapsToOtherEvent(event)) {
-  //     const preventDefault = this.passEventToHandlers(event);
-  //     if (preventDefault) {
-  //       event.preventDefault();
-  //     }
-  //   }
-  // }
-  //
-  // /**
-  //  * keyUp events are handled for simple input we take over from
-  //  * browser input.
-  //  */
-  // @action
-  // handleKeyUp(event: KeyboardEvent) {
-  //   const preventDefault = this.passEventToHandlers(event);
-  //   if (preventDefault) {
-  //     event.preventDefault();
-  //   }
-  // }
-  //
-  // /**
-  //  * reading of blogs and part of the spec seems to indicate capture input is the safest way to capture input
-  //  * this method is only called for input that hasn't been handled in earlier events (like keydown)
-  //  */
-  // @action
-  // handleInput(event: InputEvent) {
-  //   const preventDefault = this.passEventToHandlers(event);
-  //   if (preventDefault) {
-  //     event.preventDefault();
-  //   }
-  // }
-  //
-  // @action
-  // beforeInput(event: InputEvent) {
-  //   const preventDefault = this.passEventToHandlers(event);
-  //   if (preventDefault) {
-  //     event.preventDefault();
-  //   }
-  // }
-  //
-  // /**
-  //  * compositionEnd events are parsed for complex input, for uncaptured events we update
-  //  * the internal state to be inline with reality
-  //  */
-  // @action
-  // compositionEnd(event: CompositionEvent) {
-  //   const preventDefault = this.passEventToHandlers(event);
-  //   if (preventDefault) {
-  //     event.preventDefault();
-  //   }
-  // }
-  //
-  //   if (preventDefault) {
-  //     event.preventDefault();
-  //   }
-  // }
-  //
-  // @action
-  // handleMouseDown(/* event: MouseEvent */) {
-  //   // not handling just yet
-  // }
-  //
-  // /**
-  //  * dragstart isn't allowed at the moment
-  //  */
-  // @action
-  // dragstart(event: MouseEvent) {
-  //   event.preventDefault();
-  // }
-  //
-  // /**
-  //  * passes an event to handlers and returns whether the event default should be prevented or not
-  //  * @method passEventToHandlers
-  //  * @return {Boolean}
-  //  * @private
-  //  */
-  // passEventToHandlers(event: Event): boolean {
-  //   const handlers = this.inputHandlers.filter(h => h.isHandlerFor(event));
-  //   if (handlers.length > 0) {
-  //     let preventDefault = false;
-  //     for (const handler of handlers) {
-  //       const handlerResponse: HandlerResponse = handler.handleEvent(event);
-  //       if (!handlerResponse.allowBrowserDefault) {
-  //         // if one handler decided the event default (e.g. browser bubbling) should be prevented we do so.
-  //         preventDefault = true;
-  //       }
-  //       if (!handlerResponse.allowPropagation) {
-  //         // handler does not allow this event to be passed to other handlers return immediately
-  //         break;
-  //       }
-  //     }
-  //     // eslint-disable-next-line @typescript-eslint/unbound-method
-  //     void taskFor(this.rawEditor.generateDiffEvents).perform();
-  //     this.rawEditor.model.read();
-  //     return preventDefault;
-  //   }
-  //   else {
-  //     this.rawEditor.model.read();
-  //     return false;
-  //   }
-  // }
-  //
 }
--- lib/content-control-plugin/addon/content-control-plugin.ts
+++ lib/content-control-plugin/addon/content-control-plugin.ts
@@ -3,6 +3,7 @@ import EditorController from "@lblod/ember-rdfa-editor/core/editor-controller";
 import InsertHtmlCommand from "content-control-plugin/commands/insert-html-command";
 import InsertXmlCommand from "content-control-plugin/commands/insert-xml-command";
 import GetContentQuery from "content-control-plugin/queries/get-content-query";
+import SelectionCommand from "content-control-plugin/commands/selection-command";
 
 export default class ContentControlPlugin implements EditorPlugin {
   static create(): ContentControlPlugin {
@@ -17,6 +18,7 @@ export default class ContentControlPlugin implements EditorPlugin {
   async initialize(controller: EditorController) {
     controller.registerCommand(InsertHtmlCommand);
     controller.registerCommand(InsertXmlCommand);
+    controller.registerCommand(SelectionCommand);
     controller.registerQuery(GetContentQuery);
   }
 
