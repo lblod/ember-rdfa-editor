0176467b9acd627d38d781b741723eb2308f2480
abeforgit <arnebertrand@gmail.com>
2021-12-05 11:30:11 +0100

    Update ember try and drop support for old ember versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,30 +2,22 @@
 
 const getChannelURL = require('ember-source-channel-url');
 
-module.exports = async function() {
+module.exports = async function () {
   return {
     scenarios: [
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0'
-          }
-        }
-      },
       {
         name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
             'ember-source': '~3.20.5'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.24.6'
           }
         }
       },
@@ -43,7 +35,8 @@ module.exports = async function() {
           devDependencies: {
             'ember-source': await getChannelURL('beta')
           }
-        }
+        },
+        allowedToFail: true
       },
       {
         name: 'ember-canary',
@@ -51,36 +44,9 @@ module.exports = async function() {
           devDependencies: {
             'ember-source': await getChannelURL('canary')
           }
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
         },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0'
-          }
-        }
+        allowedToFail: true
       },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false
-          })
-        },
-        npm: {
-          ember: {
-            edition: 'classic'
-          }
-        }
-      }
     ]
   };
 };
