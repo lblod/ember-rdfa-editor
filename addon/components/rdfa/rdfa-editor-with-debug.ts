f7f0337b34642e73427b00bcf53495d5ce6fff07
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-01-19 13:31:56 +0100

    Added new component and testdata
    All code and testdata was copied from the dummy app to the addon. All
    things seem to work, apart from arguments to the debug component.
    Also rebase to development.

---
+++ addon/components/rdfa/rdfa-editor-with-debug.ts
@@ -0,0 +1,166 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import {tracked} from '@glimmer/tracking';
+import {inject as service} from '@ember/service';
+import RdfaDocument from "@lblod/ember-rdfa-editor/utils/rdfa/rdfa-document";
+import xmlFormat from 'xml-formatter';
+import {basicSetup, EditorState, EditorView} from "@codemirror/basic-setup";
+import {xml} from "@codemirror/lang-xml";
+import {html} from "@codemirror/lang-html";
+import sampleData from '../../config/sample-data';
+
+interface FeaturesService {
+  disable: (feature: string) => void;
+  enable: (feature: string) => void
+}
+
+export default class RdfaRdfaEditorWithDebug extends Component {
+  @tracked rdfaEditor?: RdfaDocument;
+  @tracked debug: unknown;
+  @tracked xmlDebuggerOpen = false;
+  @tracked debuggerContent = "";
+  @service features!: FeaturesService;
+  @tracked htmlDebuggerOpen = false;
+  @tracked sampleData = sampleData;
+  private unloadListener?: () => void;
+  private xmlEditor?: EditorView;
+  private htmlEditor?: EditorView;
+
+  @action
+  setup() {
+    console.log("Setup of the component");
+    console.log("This model is", this.sampleData);
+    this.unloadListener = () => {
+      this.saveEditorContentToLocalStorage();
+    };
+    window.addEventListener("beforeunload", this.unloadListener);
+  }
+
+  @action
+  teardown() {
+    console.log("Teardown of the component");
+    if (this.unloadListener) {
+      window.removeEventListener("beforeunload", this.unloadListener);
+    }
+  }
+
+  @action
+  initDebug(info: unknown) {
+    this.debug = info;
+  }
+
+  @action
+  setupXmlEditor(element: HTMLElement) {
+
+    this.xmlEditor = new EditorView({
+      state: EditorState.create({
+        extensions: [basicSetup, xml()]
+      }),
+      parent: element
+    });
+    this.xmlEditor.dispatch({changes: {from: 0, insert: this.debuggerContent}});
+  }
+
+  @action
+  setupHtmlEditor(element: HTMLElement) {
+
+    this.htmlEditor = new EditorView({
+      state: EditorState.create({
+        extensions: [basicSetup, html()]
+      }),
+      parent: element
+    });
+    this.htmlEditor.dispatch({changes: {from: 0, insert: this.debuggerContent}});
+  }
+
+  get formattedXmlContent() {
+    if (this.debuggerContent) {
+      try {
+        return xmlFormat(this.debuggerContent);
+      } catch (e) {
+        return this.debuggerContent;
+      }
+
+    }
+    return this.debuggerContent;
+  }
+
+  @action
+  rdfaEditorInit(rdfaEditor: RdfaDocument) {
+    const presetContent = localStorage.getItem("EDITOR_CONTENT") ?? "";
+    this.rdfaEditor = rdfaEditor;
+    this.rdfaEditor.setHtmlContent(presetContent);
+    const editorDone = new CustomEvent("editor-done");
+    window.dispatchEvent(editorDone);
+  }
+
+  @action
+  setDebuggerContent(content: string) {
+    this.debuggerContent = content;
+  }
+
+  @action
+  setEditorContent(type: "xml" | "html", content: string) {
+    if (this.rdfaEditor) {
+      if (type === "html") {
+        this.rdfaEditor.setHtmlContent(content);
+        this.saveEditorContentToLocalStorage();
+      } else {
+        this.rdfaEditor.xmlContent = content;
+        this.saveEditorContentToLocalStorage();
+      }
+    }
+  }
+
+  @action openContentDebugger(type: "xml" | "html") {
+    if (this.rdfaEditor) {
+      if (type === "xml") {
+        this.debuggerContent = this.rdfaEditor.xmlContentPrettified;
+        this.xmlDebuggerOpen = true;
+      } else {
+        this.debuggerContent = this.rdfaEditor.htmlContent;
+        this.htmlDebuggerOpen = true;
+      }
+    }
+  }
+
+  @action closeContentDebugger(type: "xml" | "html", save: boolean) {
+    if (type === "xml") {
+      this.debuggerContent = this.xmlEditor!.state.sliceDoc();
+      this.xmlDebuggerOpen = false;
+    } else {
+      this.debuggerContent = this.htmlEditor!.state.sliceDoc();
+      this.htmlDebuggerOpen = false;
+    }
+    if (save) {
+      const content = this.debuggerContent;
+      if (!content) {
+        //xml parser doesn't accept an empty string
+        this.setEditorContent("html", "");
+      } else {
+        this.setEditorContent(type, content);
+      }
+    }
+  }
+
+  @action
+  setPasteBehaviour(event: InputEvent) {
+    const val = (event.target as HTMLSelectElement).value;
+    if (val === "textonly") {
+      this.features.disable('editor-extended-html-paste');
+      this.features.disable('editor-html-paste');
+    } else if (val === "limited") {
+      this.features.disable('editor-extended-html-paste');
+      this.features.enable('editor-html-paste');
+    } else if (val === "full") {
+      this.features.enable('editor-extended-html-paste');
+      this.features.enable('editor-html-paste');
+    }
+  }
+
+  saveEditorContentToLocalStorage() {
+    if (this.rdfaEditor) {
+      localStorage.setItem("EDITOR_CONTENT", this.rdfaEditor.htmlContent);
+    }
+  }
+}
---
+++ app/components/rdfa/rdfa-editor-with-debug.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor/components/rdfa/rdfa-editor-with-debug';
