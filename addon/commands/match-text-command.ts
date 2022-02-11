02fbb66eae9f200c83e33bc321429c38cbcd69fe
abeforgit <arnebertrand@gmail.com>
2022-02-11 16:55:32 +0100

    Return more information from match text

--- addon/commands/match-text-command.ts
+++ addon/commands/match-text-command.ts
@@ -1,10 +1,34 @@
 import Model from '@lblod/ember-rdfa-editor/model/model';
 import Command from '@lblod/ember-rdfa-editor/commands/command';
 import ModelRange from '@lblod/ember-rdfa-editor/model/model-range';
+import ModelPosition from '@lblod/ember-rdfa-editor/model/model-position';
+
+export interface TextMatch {
+  /**
+   * The range that encompasses the matched text
+   */
+  range: ModelRange;
+  /**
+   * The matched text
+   */
+  text: string;
+  /**
+   * The matched text in the capture groups
+   */
+  groups: string[];
+  /**
+   * The index in the inputstring where the match was found (0-based)
+   */
+  index: number;
+  /**
+   * The inputstring that was matched against
+   */
+  input: string;
+}
 
 export default class MatchTextCommand extends Command<
   [ModelRange, RegExp],
-  ModelRange[]
+  TextMatch[]
 > {
   name = 'match-text';
 
@@ -12,19 +36,47 @@ export default class MatchTextCommand extends Command<
     super(model);
   }
 
-  execute(limitRange: ModelRange, regex: RegExp): ModelRange[] {
+  execute(limitRange: ModelRange, regex: RegExp): TextMatch[] {
     const { textContent, indexToPos } = limitRange.getTextContentWithMapping();
 
-    const result = [];
-    for (const match of textContent.matchAll(regex)) {
-      const matchIndex = match.index;
-      if (matchIndex !== undefined) {
-        const startPos = indexToPos(matchIndex);
-        const matchedString = match[0];
-        const endPos = indexToPos(matchIndex + matchedString.length);
-        result.push(new ModelRange(startPos, endPos));
+    const result: TextMatch[] = [];
+    if (regex.global) {
+      for (const match of textContent.matchAll(regex)) {
+        const textMatch = convertMatch(match, indexToPos);
+        if (textMatch) {
+          result.push(textMatch);
+        }
       }
+      return result;
+    } else {
+      const match = textContent.match(regex);
+      if (match) {
+        const textMatch = convertMatch(match, indexToPos);
+        if (textMatch) {
+          return [textMatch];
+        }
+      }
+      return [];
     }
-    return result;
   }
+}
+
+function convertMatch(
+  match: RegExpMatchArray,
+  indexToPos: (textIndex: number) => ModelPosition
+): TextMatch | null {
+  const matchIndex = match.index;
+  if (matchIndex !== undefined) {
+    const startPos = indexToPos(matchIndex);
+    const matchedString = match[0];
+    const endPos = indexToPos(matchIndex + matchedString.length);
+    return {
+      input: match.input || '',
+      text: matchedString,
+      groups: match.slice(1),
+      range: new ModelRange(startPos, endPos),
+      index: matchIndex,
+    };
+  }
+  return null;
 }
