52d7d611dbcb7d42dccd5fe014583ccb18d86fe8
Niels Vandekeybus <progster@gmail.com>
2022-02-22 12:18:15 +0100

    whitespacing conversion when importing html

--- addon/model/readers/html-reader.ts
+++ addon/model/readers/html-reader.ts
@@ -11,16 +11,18 @@ export class HtmlReaderContext {
   private readonly _textAttributes: Map<string, string>;
   private readonly _model: Model;
   private _rdfaPrefixes: Map<string, string>;
-
+  private _shouldConvertWhitespace:boolean;
   activeMarks: Set<SpecAttributes> = new Set<SpecAttributes>();
 
   constructor(
     model: Model,
-    rdfaPrefixes: Map<string, string> = new Map<string, string>()
+    rdfaPrefixes: Map<string, string> = new Map<string, string>(),
+    shouldConvertWhitespace:boolean = false
   ) {
     this._textAttributes = new Map<string, string>();
     this._model = model;
     this._rdfaPrefixes = rdfaPrefixes;
+    this._shouldConvertWhitespace = shouldConvertWhitespace;
   }
 
   get model() {
@@ -29,6 +31,10 @@ export class HtmlReaderContext {
 
   bindNode(modelNode: ModelNode, domNode: Node) {
     this.model.bindNode(modelNode, domNode);
+  }
+
+  get shouldConvertWhitespace() {
+    return this._shouldConvertWhitespace;
   }
 
   get textAttributes() {
@@ -56,12 +62,12 @@ export class HtmlReaderContext {
  * Top-level reader for HTML documents
  */
 export default class HtmlReader implements Reader<Node, ModelNode[], void> {
-  constructor(private model: Model) {}
+  constructor(private model: Model, private shouldConvertWhitespace: boolean = false) {}
 
   read(from: Node): ModelNode[] {
     from.normalize();
     const prefixes = calculateRdfaPrefixes(from);
-    const context = new HtmlReaderContext(this.model, prefixes);
+    const context = new HtmlReaderContext(this.model, prefixes, this.shouldConvertWhitespace);
     const nodeReader = new HtmlNodeReader();
     return nodeReader.read(from, context);
   }
--- addon/model/readers/html-text-reader.ts
+++ addon/model/readers/html-text-reader.ts
@@ -1,6 +1,7 @@
 import Reader from '@lblod/ember-rdfa-editor/model/readers/reader';
 import ModelText from '@lblod/ember-rdfa-editor/model/model-text';
 import { HtmlReaderContext } from '@lblod/ember-rdfa-editor/model/readers/html-reader';
+import { normalToPreWrapWhiteSpace } from '@lblod/ember-rdfa-editor/utils/whitespace-collapsing';
 
 /**
  * Reader responsible for reading HTML Text nodes
@@ -12,19 +13,12 @@ export default class HtmlTextReader
     if (!from.textContent) {
       return [];
     }
-    let trimmed = from.textContent;
-    // Use ECMA-262 Edition 3 String and RegExp features
-    // trimmed = trimmed.replace(/[\t\n\r ]+/g, ' ');
-    const pattern = /[\f\n\r\t\v ]{2,}/g;
-    const replacement = ' ';
 
-    trimmed = trimmed.replace(pattern, replacement);
-    if (trimmed.charAt(0) == ' ') {
-      trimmed = trimmed.substring(1, trimmed.length);
+    let trimmed = from.textContent;
+    if (context.shouldConvertWhitespace) {
+      trimmed = normalToPreWrapWhiteSpace(trimmed);
     }
-    if (!trimmed.length) {
-      return [];
-    }
+
     const result = new ModelText(trimmed);
     context.activeMarks.forEach(({ spec, attributes }) =>
       context.addMark(result, spec, attributes)
