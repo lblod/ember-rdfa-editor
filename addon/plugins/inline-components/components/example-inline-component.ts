208f8bfce985d93e76f94d4004b6440799a0cc80
Elena Poelman <elena.poelman@redpencil.io>
2022-05-23 09:28:46 +0200

    removed second image

--- addon/plugins/inline-components/components/example-inline-component.ts
+++ addon/plugins/inline-components/components/example-inline-component.ts
@@ -44,10 +44,6 @@ export const exampleInlineComponent: InlineComponent = {
           imageSrc:
             'https://emberjs.com/images/tomsters/a11y-tomster750w-2137b8b891485dd920ccf5c0d5d1645d.png',
         }),
-        imageInlineComponent.render({
-          imageSrc:
-            'https://emberjs.com/images/tomsters/tomster750w-64ed66ab3d6aaa63ed0538b9cf958dbb.png',
-        }),
       ],
     };
   },
