1108168e71bdac37750016589b5a30ba4c951d4b
Elena Poelman <elena.poelman@redpencil.io>
2022-05-31 14:32:09 +0200

    replaced attachment content by dummy content

--- addon/components/inline-components/regulatory-attachment.ts
+++ addon/components/inline-components/regulatory-attachment.ts
@@ -2,19 +2,10 @@ import Component from '@glimmer/component';
 
 export default class InlineComponentsRegulatoryAttachment extends Component {
   get title() {
-    return 'Subsidiereglement voor het organiseren van buurtactiviteiten';
+    return 'Example Regulatory Attachment';
   }
 
   get articles() {
-    return [
-      'Doel',
-      'Definities',
-      'Welke kosten komen in aanmerking voor subsidiëring?',
-      'Welke kosten komen niet in aanmerking voor subsidiëring?',
-      'Doelgroep',
-      'Welke activiteiten zijn uitgesloten voor het bekomen van deze subsidie?',
-      'Algemene voorwaarden',
-      'Procedure aanvraag',
-    ];
+    return [1, 2, 3, 4, 5, 6, 7].map((i) => `Article ${i}`);
   }
 }
