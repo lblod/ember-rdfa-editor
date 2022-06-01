453246c579d6ddc431f1657a0a89f23ad074141c
abeforgit <arnebertrand@gmail.com>
2022-06-01 21:44:49 +0200

    Document the view interface

--- addon/core/view.ts
+++ addon/core/view.ts
@@ -7,16 +7,38 @@ import { domPosToModelPos, modelPosToDomPos } from '../utils/dom-helpers';
 import { PositionError } from '../utils/errors';
 import { createLogger, Logger } from '../utils/logging-utils';
 
+/**
+ * This interface controls the interaction with the html DOM
+ * It doesn't hold state of its own (well, or very little), but simply manages
+ * updating the representation of any state that it gets on the dom.
+ * */
 export interface View {
+  /**
+   * The html element that the editor will render in
+   * */
   domRoot: Element;
 
+  /**
+   * Get the domNode that corresponds to the given modelNode
+   * State is needed because active configuration may influence the result
+   * */
   modelToView(state: State, modelNode: ModelNode): Node | null;
 
+  /**
+   * Get the modelnode that corresponds tot the given domNode
+   * State is needed because active configuration may influence the result
+   * */
   viewToModel(state: State, domNode: Node): ModelNode;
 
+  /**
+   * Update the DOM to represent the given state
+   * */
   update(state: State): void;
 }
 
+/**
+ * Default implementation of the view interface
+ * */
 export class EditorView implements View {
   domRoot: Element;
   logger: Logger;
