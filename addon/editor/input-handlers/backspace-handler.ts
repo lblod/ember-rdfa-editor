b95f9c2628c4dda1c7be6e6f003d626e10ae0092
Elena Poelman <elena.poelman@redpencil.io>
2022-07-04 15:23:44 +0200

    when the next node in the direction is an rdfa element move cursor instead of applying deletion in backspace and delete handlers

--- addon/editor/input-handlers/backspace-handler.ts
+++ addon/editor/input-handlers/backspace-handler.ts
@@ -1,6 +1,12 @@
 import { InputHandler } from './input-handler';
 import RawEditor from '@lblod/ember-rdfa-editor/utils/ce/raw-editor';
 import { isKeyDownEvent } from '@lblod/ember-rdfa-editor/editor/input-handlers/event-helpers';
+import ModelRange from '@lblod/ember-rdfa-editor/model/model-range';
+import ModelPosition from '@lblod/ember-rdfa-editor/model/model-position';
+import ModelNode from '@lblod/ember-rdfa-editor/model/model-node';
+import ModelNodeUtils from '@lblod/ember-rdfa-editor/model/util/model-node-utils';
+import GenTreeWalker from '@lblod/ember-rdfa-editor/model/util/gen-tree-walker';
+import { toFilterSkipFalse } from '@lblod/ember-rdfa-editor/model/util/model-tree-walker';
 
 /**
  * EnterHandler, an event handler to handle the generic enter case.
@@ -19,7 +25,28 @@ export default class BackspaceHandler extends InputHandler {
   }
 
   handleEvent(_: KeyboardEvent) {
-    this.rawEditor.executeCommand('remove', 'left');
+    const range = this.rawEditor.selection.lastRange;
+    if (range) {
+      const newStart = range.start.shiftedVisually(-1);
+      const newEnd = range.start;
+      const removeRange = new ModelRange(newStart, newEnd);
+      const nextNode = GenTreeWalker.fromRange({
+        range: removeRange,
+        filter: toFilterSkipFalse((node) => ModelNode.isModelElement(node)),
+        reverse: true,
+      }).nextNode();
+      if (
+        !ModelNode.isModelElement(nextNode) ||
+        nextNode.getRdfaAttributes().isEmpty
+      ) {
+        this.rawEditor.executeCommand('remove', 'left');
+      } else {
+        this.rawEditor.model.change(() => {
+          this.rawEditor.model.selectRange(new ModelRange(newEnd));
+        });
+      }
+    }
+
     return { allowPropagation: true, allowBrowserDefault: false };
   }
 }
--- addon/editor/input-handlers/delete-handler.ts
+++ addon/editor/input-handlers/delete-handler.ts
@@ -1,6 +1,10 @@
 import { InputHandler } from './input-handler';
 import RawEditor from '@lblod/ember-rdfa-editor/utils/ce/raw-editor';
 import { isKeyDownEvent } from '@lblod/ember-rdfa-editor/editor/input-handlers/event-helpers';
+import ModelNode from '@lblod/ember-rdfa-editor/model/model-node';
+import ModelRange from '@lblod/ember-rdfa-editor/model/model-range';
+import GenTreeWalker from '@lblod/ember-rdfa-editor/model/util/gen-tree-walker';
+import { toFilterSkipFalse } from '@lblod/ember-rdfa-editor/model/util/model-tree-walker';
 
 /**
  * EnterHandler, an event handler to handle the generic enter case.
@@ -19,8 +23,26 @@ export default class DeleteHandler extends InputHandler {
   }
 
   handleEvent(_: KeyboardEvent) {
-    this.rawEditor.executeCommand('remove', 'right');
-    console.log('DELETE AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+    const range = this.rawEditor.selection.lastRange;
+    if (range) {
+      const newStart = range.start;
+      const newEnd = range.start.shiftedVisually(1);
+      const removeRange = new ModelRange(newStart, newEnd);
+      const nextNode = GenTreeWalker.fromRange({
+        range: removeRange,
+        filter: toFilterSkipFalse((node) => ModelNode.isModelElement(node)),
+      }).nextNode();
+      if (
+        !ModelNode.isModelElement(nextNode) ||
+        nextNode.getRdfaAttributes().isEmpty
+      ) {
+        this.rawEditor.executeCommand('remove', 'right');
+      } else {
+        this.rawEditor.model.change(() => {
+          this.rawEditor.model.selectRange(new ModelRange(newEnd));
+        });
+      }
+    }
     return { allowPropagation: true, allowBrowserDefault: false };
   }
 }
