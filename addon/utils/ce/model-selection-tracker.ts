170568078405bc8431caf415a2a8a27d7e031328
abeforgit <arnebertrand@gmail.com>
2022-07-11 08:23:15 +0200

    Remove erroneous duplicate event check
    The check was never correct in the first place

--- addon/utils/ce/model-selection-tracker.ts
+++ addon/utils/ce/model-selection-tracker.ts
@@ -2,19 +2,8 @@ import Model from '@lblod/ember-rdfa-editor/model/model';
 import { getWindowSelection } from '@lblod/ember-rdfa-editor/utils/dom-helpers';
 import { createLogger, Logger } from '../logging-utils';
 
-interface DomSelection {
-  anchorNode?: Node;
-
-  focusNode?: Node;
-
-  anchorOffset: number;
-
-  focusOffset: number;
-}
-
 export default class ModelSelectionTracker {
   model: Model;
-  previousSelection: DomSelection | null = null;
   logger: Logger;
 
   constructor(model: Model) {
@@ -32,9 +21,6 @@ export default class ModelSelectionTracker {
 
   updateSelection = () => {
     const currentSelection = getWindowSelection();
-    if (this.isSameAsPrevious(currentSelection)) {
-      return;
-    }
     if (
       !this.model.rootNode.contains(currentSelection.anchorNode) ||
       !this.model.rootNode.contains(currentSelection.focusNode) ||
@@ -44,25 +30,7 @@ export default class ModelSelectionTracker {
     ) {
       return;
     }
-    this.previousSelection = {
-      anchorOffset: currentSelection.anchorOffset,
-      focusNode: currentSelection.focusNode?.cloneNode(),
-      anchorNode: currentSelection.anchorNode?.cloneNode(),
-      focusOffset: currentSelection.focusOffset,
-    };
     this.logger('Dom selection updated, recalculating selection');
     this.model.readSelection();
   };
-
-  isSameAsPrevious(selection: Selection) {
-    if (!this.previousSelection) {
-      return false;
-    }
-    return (
-      selection.anchorNode === this.previousSelection.anchorNode &&
-      selection.focusNode === this.previousSelection.focusNode &&
-      selection.anchorOffset === this.previousSelection.anchorOffset &&
-      selection.focusOffset === this.previousSelection.focusOffset
-    );
-  }
 }
