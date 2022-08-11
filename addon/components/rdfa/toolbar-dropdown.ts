fd015f4b6bc66e7c327092f337ab8ef7d2aa429a
abeforgit <arnebertrand@gmail.com>
2022-08-11 20:46:07 +0200

    Admit defeat to the focus-trap hack

--- addon/components/rdfa/toolbar-dropdown.ts
+++ addon/components/rdfa/toolbar-dropdown.ts
@@ -31,10 +31,14 @@ export default class AuDropdown extends Component<Args> {
     // some kind of focus event always seems to happen at the wrong time
     // so this is a bit of hack, but it works well.
     await paintCycleHappened();
+    // I have no idea why this particular hack works, I've tried it
+    // just about any other way but this is the only one that works
+    // focus-trap is the bane of my existence
+    // SAFETY: if domRoot is ever an Element that's not an HTMLElement,
+    // then we're definitely not in Kansas anymore
     this.args.controller.perform(() => {
       (this.args.controller.view.domRoot as HTMLElement).focus();
     });
-    // this.args.editor.model.writeSelection();
     return true;
   }
 }
