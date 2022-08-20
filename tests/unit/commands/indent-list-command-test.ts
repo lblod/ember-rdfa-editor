adbb48d7271a73cbba626eea64d71c5a578192b1
abeforgit <arnebertrand@gmail.com>
2022-08-20 03:51:36 +0200

    test(lists): add failing tests for selection behavior

--- tests/unit/commands/indent-list-command-test.ts
+++ tests/unit/commands/indent-list-command-test.ts
@@ -46,4 +46,103 @@ module('Unit | commands | indent-list-command-test', function () {
       </div>`;
     assert.true(resultState.document.sameAs(expected));
   });
+  test('selection stays in indented list', function (assert) {
+    // language=XML
+    const {
+      root: initial,
+      textNodes: { iniContent },
+    } = vdom`
+      <div>
+        <ul>
+          <li>
+            <text>abc</text>
+          </li>
+          <li>
+            <!--                    |1-->
+            <text __id="iniContent">def</text>
+          </li>
+        </ul>
+      </div>`;
+
+    const start = ModelPosition.fromInTextNode(iniContent, 3);
+    const range1 = new ModelRange(start, start);
+    const initialState = stateWithRange(initial, range1);
+    const { resultState } = executeCommand(initialState, {});
+    // language=XML
+    const {
+      root: expected,
+      textNodes: { expContent },
+    } = vdom`
+      <div>
+        <ul>
+          <li>
+            <text>abc</text>
+            <ul>
+              <li>
+                <!--                       |2-->
+                <text __id="expContent">def</text>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>`;
+    const range2 = ModelRange.fromInTextNode(expContent, 3, 3);
+    assert.true(
+      resultState.document.sameAs(expected),
+      QUnit.dump.parse(resultState.document)
+    );
+    assert.true(
+      resultState.selection.lastRange!.sameAs(range2),
+      `range1: ${resultState.selection.lastRange!.toString()}, range2: ${range2.toString()}`
+    );
+  });
+  test('uncollapsed selection stays in indented list', function (assert) {
+    // language=XML
+    const {
+      root: initial,
+      textNodes: { iniContent },
+    } = vdom`
+      <div>
+        <ul>
+          <li>
+            <text>abc</text>
+          </li>
+          <li>
+            <!--                    [1]-->
+            <text __id="iniContent">def</text>
+          </li>
+        </ul>
+      </div>`;
+
+    const range1 = ModelRange.fromInTextNode(iniContent, 0, 3);
+    const initialState = stateWithRange(initial, range1);
+    const { resultState } = executeCommand(initialState, {});
+    // language=XML
+    const {
+      root: expected,
+      textNodes: { expContent },
+    } = vdom`
+      <div>
+        <ul>
+          <li>
+            <text>abc</text>
+            <ul>
+              <li>
+                <!--                    [2]-->
+                <text __id="expContent">def</text>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>`;
+    const range2 = ModelRange.fromInTextNode(expContent, 0, 3);
+    assert.true(
+      resultState.document.sameAs(expected),
+      QUnit.dump.parse(resultState.document)
+    );
+    assert.true(
+      resultState.selection.lastRange!.sameAs(range2),
+      `range1: ${resultState.selection.lastRange!.toString()}, range2: ${range2.toString()}`
+    );
+  });
 });
