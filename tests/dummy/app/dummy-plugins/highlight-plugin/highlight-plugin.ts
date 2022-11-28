82b3dde900b68612160c6811d99cfecc4cdaccc7
Elena Poelman <elena.poelman@redpencil.io>
2022-11-28 21:10:00 +0100

    remove code duplication in highlight plugin

--- tests/dummy/app/dummy-plugins/highlight-plugin/highlight-plugin.ts
+++ tests/dummy/app/dummy-plugins/highlight-plugin/highlight-plugin.ts
@@ -3,36 +3,40 @@ import {
   Logger,
 } from '@lblod/ember-rdfa-editor/utils/logging-utils';
 import RdfaEditorPlugin from '@lblod/ember-rdfa-editor/core/rdfa-editor-plugin';
-import { InputRule, inputRules } from 'prosemirror-inputrules';
 import { EditorState, Plugin, Transaction } from 'prosemirror-state';
 import { Decoration, DecorationSet } from 'prosemirror-view';
+import { Node as PNode } from 'prosemirror-model';
 export interface HighlightPluginOptions {
   testKey: string;
 }
 
-function placeholder(): Plugin {
-  const placeholder: Plugin<DecorationSet> = new Plugin<DecorationSet>({
+function calculateDecorations(doc: PNode) {
+  const ranges: { start: number; end: number }[] = [];
+  const decorations: Decoration[] = [];
+  doc.descendants((node, pos) => {
+    if (node.isText && node.text) {
+      for (const match of node.text.matchAll(/test/g)) {
+        ranges.push({
+          start: pos + match.index!,
+          end: pos + match.index! + 4,
+        });
+        decorations.push(
+          Decoration.inline(pos + match.index!, pos + match.index! + 4, {
+            style: 'background: yellow',
+          })
+        );
+      }
+    }
+  });
+  return DecorationSet.create(doc, decorations);
+}
+
+function highlight(): Plugin {
+  const highlight: Plugin<DecorationSet> = new Plugin<DecorationSet>({
     state: {
       init(_, state: EditorState) {
         const { doc } = state;
-        const ranges: { start: number; end: number }[] = [];
-        doc.descendants((node, pos) => {
-          if (node.isText && node.text) {
-            const regexp = /test/g;
-            let match: RegExpExecArray | null;
-            while ((match = regexp.exec(node.text)) !== null) {
-              ranges.push({
-                start: pos + match.index,
-                end: pos + match.index + 4,
-              });
-            }
-          }
-        });
-
-        const decorations = ranges.map(({ start, end }) =>
-          Decoration.inline(start, end, { style: 'background: yellow' })
-        );
-        return DecorationSet.create(doc, decorations);
+        return calculateDecorations(doc);
       },
       apply(
         tr: Transaction,
@@ -41,54 +45,16 @@ function placeholder(): Plugin {
         newState: EditorState
       ) {
         const { doc } = newState;
-        const ranges: { start: number; end: number }[] = [];
-        doc.descendants((node, pos) => {
-          if (node.isText && node.text) {
-            for (const match of node.text.matchAll(/test/g)) {
-              ranges.push({
-                start: pos + match.index!,
-                end: pos + match.index! + 4,
-              });
-            }
-          }
-        });
-
-        const decorations = ranges.map(({ start, end }) =>
-          Decoration.inline(start, end, { style: 'background: yellow' })
-        );
-        return DecorationSet.create(doc, decorations);
+        return calculateDecorations(doc);
       },
     },
     props: {
       decorations(state: EditorState) {
-        return placeholder.getState(state);
+        return highlight.getState(state);
       },
     },
   });
-  // props: {
-  //   decorations: (state) => {
-  //     const decorations: Decoration[] = [];
-  //
-  //     const decorate = (node: PNode, pos: number) => {
-  //       if (
-  //         node.type.isBlock &&
-  //         node.childCount === 0 &&
-  //         state.selection.$anchor.parent !== node
-  //       ) {
-  //         decorations.push(
-  //           Decoration.node(pos, pos + node.nodeSize, {
-  //             class: 'empty-node',
-  //           })
-  //         );
-  //       }
-  //     };
-  //
-  //     state.doc.descendants(decorate);
-  //
-  //     return DecorationSet.create(state.doc, decorations);
-  //   },
-  // },
-  return placeholder;
+  return highlight;
 }
 
 export default class HighlightPlugin extends RdfaEditorPlugin {
@@ -99,6 +65,6 @@ export default class HighlightPlugin extends RdfaEditorPlugin {
   }
 
   proseMirrorPlugins(): Plugin[] {
-    return [placeholder()];
+    return [highlight()];
   }
 }
