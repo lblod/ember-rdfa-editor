6f6f22098292b0c9ec95bb5d3afc36dea05f005f
abeforgit <arnebertrand@gmail.com>
2022-12-09 20:15:27 +0100

    Export more types add more datastore utils

--- addon/index.ts
+++ addon/index.ts
@@ -1,21 +1,21 @@
+import { Decoration } from 'prosemirror-view';
+
 export {
+  WidgetSpec,
+  ProseController,
   EmberInlineComponent,
   emberComponent,
+  default as ProseMirror,
+  EmberInlineComponentArgs,
+  WidgetLocation,
 } from '@lblod/ember-rdfa-editor/core/prosemirror';
+export * from 'prosemirror-model';
+export { Node as PNode } from 'prosemirror-model';
 
-export { WidgetSpec } from '@lblod/ember-rdfa-editor/core/prosemirror';
-export { Node as PNode, NodeSpec, MarkSpec } from 'prosemirror-model';
 export { getRdfaAttrs, rdfaAttrs } from '@lblod/ember-rdfa-editor/core/schema';
-export {
-  Plugin as ProsePlugin,
-  PluginKey,
-  EditorState,
-  Transaction,
-} from 'prosemirror-state';
-export {
-  EditorView,
-  DecorationSet,
-  Decoration,
-  NodeView,
-  NodeViewConstructor,
-} from 'prosemirror-view';
+export * from 'prosemirror-state';
+export { Plugin as ProsePlugin } from 'prosemirror-state';
+export * from 'prosemirror-view';
+export type InlineDecorationSpec = NonNullable<
+  Parameters<typeof Decoration.inline>[3]
+>;
