064083ef8a58c1b74a316d52fb02cc1ad6deecab
Elena Poelman <elena.poelman@redpencil.io>
2022-12-13 09:52:53 +0100

    add initial version of nodes-between function

--- addon/utils/position-utils.ts
+++ addon/utils/position-utils.ts
@@ -15,3 +15,69 @@ export function findAncestors(
   }
   return result;
 }
+
+export function* children(
+  resolvedNode: { node: PNode; pos: number },
+  reverse = true,
+  recursive = true,
+  filter: (node: PNode) => boolean = () => true,
+  startIndex = reverse ? resolvedNode.node.childCount - 1 : 0
+): Generator<{ node: PNode; pos: number }, void> {
+  const { node, pos } = resolvedNode;
+  if (reverse) {
+    let offset = node.content.size;
+    for (let i = startIndex; i >= 0; i--) {
+      offset -= node.child(i).nodeSize;
+      const resolvedChild = { node: node.child(i), pos: pos + 1 + offset };
+      if (recursive) {
+        yield* children(resolvedChild, reverse, recursive, filter);
+      }
+      if (filter(node.child(i))) {
+        yield resolvedChild;
+      }
+    }
+  } else {
+    let offset = 0;
+    for (let i = 0; i < node.childCount; i++) {
+      if (i >= startIndex) {
+        const resolvedChild = { node: node.child(i), pos: pos + 1 + offset };
+        if (filter(resolvedChild.node)) {
+          yield { node: node.child(i), pos: pos + 1 + offset };
+        }
+        if (recursive) {
+          yield* children(resolvedChild, reverse, recursive, filter);
+        }
+      }
+      offset += node.child(i).nodeSize;
+    }
+  }
+}
+
+export function* nodesBetween(
+  from: ResolvedPos,
+  visitParentUpwards = false,
+  reverse = false,
+  filter: (node: PNode) => boolean = () => true
+): Generator<{ node: PNode; pos: number }, undefined> {
+  let startIndex: number;
+  const index = from.index();
+  const indexAfter = from.indexAfter();
+  if (reverse) {
+    startIndex = index === indexAfter ? index - 1 : index;
+  } else {
+    startIndex = index;
+  }
+  const resolvedParent = {
+    node: from.parent,
+    pos: from.depth > 0 ? from.before() : -1,
+  };
+  yield* children(resolvedParent, reverse, true, filter, startIndex);
+  if (visitParentUpwards && from.depth !== 0) {
+    yield resolvedParent;
+    const resolvedPos = from.doc.resolve(
+      reverse ? from.before() : from.after()
+    );
+    yield* nodesBetween(resolvedPos, visitParentUpwards, reverse, filter);
+  }
+  return;
+}
---
+++ tests/unit/utils/position-utils-test.ts
@@ -0,0 +1,171 @@
+import { module, test } from 'qunit';
+import TEST_SCHEMA from 'dummy/tests/test-utils';
+import { nodesBetween } from '@lblod/ember-rdfa-editor/utils/position-utils';
+
+module('Unit | utils | position-utils | nodes-between', function () {
+  // Replace this with your real tests.
+  test('Simple document', function (assert) {
+    const schema = TEST_SCHEMA;
+    const doc = schema.node('doc', {}, [
+      schema.node('paragraph', null, [
+        schema.text('abc'),
+        schema.node('inline_rdfa', null, [schema.text('def')]),
+        schema.text('ghi'),
+      ]),
+    ]);
+    const start = doc.resolve(0);
+    const iterator = nodesBetween(start);
+
+    const values = [...iterator];
+    assert.strictEqual(values.length, 5);
+
+    assert.strictEqual(values[0].node.type.name, 'paragraph');
+    assert.strictEqual(values[0].pos, 0);
+
+    assert.strictEqual(values[1].node.text, 'abc');
+    assert.strictEqual(values[1].pos, 1);
+
+    assert.strictEqual(values[2].node.type.name, 'inline_rdfa');
+    assert.strictEqual(values[2].pos, 4);
+
+    assert.strictEqual(values[3].node.text, 'def');
+    assert.strictEqual(values[3].pos, 5);
+
+    assert.strictEqual(values[4].node.text, 'ghi');
+    assert.strictEqual(values[4].pos, 9);
+  });
+  test('Simple document - reverse', function (assert) {
+    const schema = TEST_SCHEMA;
+    const doc = schema.node('doc', {}, [
+      schema.node('paragraph', null, [
+        schema.text('abc'),
+        schema.node('inline_rdfa', null, [schema.text('def')]),
+        schema.text('ghi'),
+      ]),
+    ]);
+    const start = doc.resolve(doc.content.size);
+    const iterator = nodesBetween(start, true, true);
+
+    const values = [...iterator];
+    assert.strictEqual(values.length, 5);
+
+    assert.strictEqual(values[0].node.text, 'ghi');
+    assert.strictEqual(values[0].pos, 9);
+
+    assert.strictEqual(values[1].node.text, 'def');
+    assert.strictEqual(values[1].pos, 5);
+
+    assert.strictEqual(values[2].node.type.name, 'inline_rdfa');
+    assert.strictEqual(values[2].pos, 4);
+
+    assert.strictEqual(values[3].node.text, 'abc');
+    assert.strictEqual(values[3].pos, 1);
+
+    assert.strictEqual(values[4].node.type.name, 'paragraph');
+    assert.strictEqual(values[4].pos, 0);
+  });
+
+  test('Simple document with text condition', function (assert) {
+    const schema = TEST_SCHEMA;
+    const doc = schema.node('doc', {}, [
+      schema.node('paragraph', null, [
+        schema.text('abc'),
+        schema.node('inline_rdfa', null, [schema.text('def')]),
+        schema.text('ghi'),
+      ]),
+    ]);
+    const start = doc.resolve(0);
+    const iterator = nodesBetween(start, true, false, (node) => node.isText);
+
+    const values = [...iterator];
+    assert.strictEqual(values.length, 3);
+
+    assert.strictEqual(values[0].node.text, 'abc');
+    assert.strictEqual(values[0].pos, 1);
+
+    assert.strictEqual(values[1].node.text, 'def');
+    assert.strictEqual(values[1].pos, 5);
+
+    assert.strictEqual(values[2].node.text, 'ghi');
+    assert.strictEqual(values[2].pos, 9);
+  });
+  test('Simple document in reverse with text condition', function (assert) {
+    const schema = TEST_SCHEMA;
+    const doc = schema.node('doc', {}, [
+      schema.node('paragraph', null, [
+        schema.text('abc'),
+        schema.node('inline_rdfa', null, [schema.text('def')]),
+        schema.text('ghi'),
+      ]),
+    ]);
+    const start = doc.resolve(doc.content.size);
+    const iterator = nodesBetween(start, true, true, (node) => node.isText);
+
+    const values = [...iterator];
+    assert.strictEqual(values.length, 3);
+    assert.strictEqual(values[2].node.text, 'abc');
+    assert.strictEqual(values[2].pos, 1);
+
+    assert.strictEqual(values[1].node.text, 'def');
+    assert.strictEqual(values[1].pos, 5);
+
+    assert.strictEqual(values[0].node.text, 'ghi');
+    assert.strictEqual(values[0].pos, 9);
+  });
+  test('Simple document with start inside text node', function (assert) {
+    const schema = TEST_SCHEMA;
+    const doc = schema.node('doc', {}, [
+      schema.node('paragraph', null, [
+        schema.text('abc'),
+        schema.node('inline_rdfa', null, [schema.text('def')]),
+        schema.text('ghi'),
+      ]),
+    ]);
+    const start = doc.resolve(2);
+    const iterator = nodesBetween(start);
+
+    const values = [...iterator];
+    assert.strictEqual(values.length, 4);
+    assert.strictEqual(values[0].node.text, 'abc');
+    assert.strictEqual(values[0].pos, 1);
+
+    assert.strictEqual(values[1].node.type.name, 'inline_rdfa');
+    assert.strictEqual(values[1].pos, 4);
+
+    assert.strictEqual(values[2].node.text, 'def');
+    assert.strictEqual(values[2].pos, 5);
+
+    assert.strictEqual(values[3].node.text, 'ghi');
+    assert.strictEqual(values[3].pos, 9);
+
+  });
+  test('Simple document with start inside text node - reverse', function (assert) {
+    const schema = TEST_SCHEMA;
+    const doc = schema.node('doc', {}, [
+      schema.node('paragraph', null, [
+        schema.text('abc'),
+        schema.node('inline_rdfa', null, [schema.text('def')]),
+        schema.text('ghi'),
+      ]),
+    ]);
+    const start = doc.resolve(10);
+    const iterator = nodesBetween(start, true, true);
+
+    const values = [...iterator];
+    assert.strictEqual(values.length, 5);
+    assert.strictEqual(values[0].node.text, 'ghi');
+    assert.strictEqual(values[0].pos, 9);
+
+    assert.strictEqual(values[1].node.text, 'def');
+    assert.strictEqual(values[1].pos, 5);
+
+    assert.strictEqual(values[2].node.type.name, 'inline_rdfa');
+    assert.strictEqual(values[2].pos, 4);
+
+    assert.strictEqual(values[3].node.text, 'abc');
+    assert.strictEqual(values[3].pos, 1);
+
+    assert.strictEqual(values[4].node.type.name, 'paragraph');
+    assert.strictEqual(values[4].pos, 0);
+  });
+});
