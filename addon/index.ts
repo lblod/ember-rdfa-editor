f3c5b7a0cd4d47bd8f63854460f133962f5ee2ca
Elena Poelman <elena.poelman@redpencil.io>
2022-12-13 11:31:57 +0100

    add additional exports from prosemirror-state

--- addon/index.ts
+++ addon/index.ts
@@ -7,11 +7,16 @@ export {
   WidgetLocation,
 } from '@lblod/ember-rdfa-editor/core/prosemirror';
 export * from 'prosemirror-model';
-export { Node as PNode } from 'prosemirror-model';
+export { Node as PNode, DOMParser as ProseParser } from 'prosemirror-model';
 
 export { getRdfaAttrs, rdfaAttrs } from '@lblod/ember-rdfa-editor/core/schema';
 export * from 'prosemirror-state';
-export { Plugin as ProsePlugin } from 'prosemirror-state';
+export {
+  Plugin as ProsePlugin,
+  Selection,
+  TextSelection,
+  Command,
+} from 'prosemirror-state';
 export * from 'prosemirror-view';
 export type InlineDecorationSpec = NonNullable<
   Parameters<typeof Decoration.inline>[3]
