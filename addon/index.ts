37ebcbd3735d311d2e842a1c9b749fc5dcdbb1bd
abeforgit <arnebertrand@gmail.com>
2022-12-14 17:20:46 +0100

    fix(build): export all needed things from editor index

--- addon/index.ts
+++ addon/index.ts
@@ -1,23 +1,26 @@
 import { Decoration } from 'prosemirror-view';
 
+export { getRdfaAttrs, rdfaAttrs } from '@lblod/ember-rdfa-editor/core/schema';
+
 export {
   WidgetSpec,
   ProseController,
   default as ProseMirror,
   WidgetLocation,
 } from '@lblod/ember-rdfa-editor/core/prosemirror';
+
 export * from 'prosemirror-model';
 export { Node as PNode, DOMParser as ProseParser } from 'prosemirror-model';
 
-export { getRdfaAttrs, rdfaAttrs } from '@lblod/ember-rdfa-editor/core/schema';
 export * from 'prosemirror-state';
-export {
-  Plugin as ProsePlugin,
-  Selection,
-  TextSelection,
-  Command,
-} from 'prosemirror-state';
+export { Plugin as ProsePlugin } from 'prosemirror-state';
+
 export * from 'prosemirror-view';
+
+export * from 'prosemirror-commands';
+
+export * from 'prosemirror-transform';
+
 export type InlineDecorationSpec = NonNullable<
   Parameters<typeof Decoration.inline>[3]
 >;
