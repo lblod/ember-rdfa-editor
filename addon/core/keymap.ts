b13b3b0e4d64af797ce5a8edc043f5e0d2426a3e
abeforgit <arnebertrand@gmail.com>
2022-12-20 20:29:56 +0100

    feat(marks): make mark behavior the same as before

--- addon/core/keymap.ts
+++ addon/core/keymap.ts
@@ -2,7 +2,7 @@ import { redo, undo } from 'prosemirror-history';
 import { splitListItem } from 'prosemirror-schema-list';
 import { Command } from 'prosemirror-state';
 import { Schema } from 'prosemirror-model';
-import { toggleMark } from 'prosemirror-commands';
+import { toggleMarkAddFirst } from '@lblod/ember-rdfa-editor/commands/toggle-mark-add-first';
 
 export type Keymap = (schema: Schema) => Record<string, Command>;
 
@@ -10,9 +10,9 @@ export function defaultKeymap(schema: Schema): Record<string, Command> {
   return {
     'Ctrl-z': undo,
     'Ctrl-Shift-z': redo,
-    'Ctrl-b': toggleMark(schema.marks['strong']),
-    'Ctrl-i': toggleMark(schema.marks['em']),
-    'Ctrl-u': toggleMark(schema.marks['underline']),
+    'Ctrl-b': toggleMarkAddFirst(schema.marks['strong']),
+    'Ctrl-i': toggleMarkAddFirst(schema.marks['em']),
+    'Ctrl-u': toggleMarkAddFirst(schema.marks['underline']),
     Enter: splitListItem(schema.nodes.list_item),
   };
 }
