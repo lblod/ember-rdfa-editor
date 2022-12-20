2465305c6487508b85d05581a836c173596a77d6
abeforgit <arnebertrand@gmail.com>
2022-12-20 16:10:33 +0100

    feat(styles): hookup common text style shortcuts

--- addon/core/keymap.ts
+++ addon/core/keymap.ts
@@ -2,6 +2,7 @@ import { redo, undo } from 'prosemirror-history';
 import { splitListItem } from 'prosemirror-schema-list';
 import { Command } from 'prosemirror-state';
 import { Schema } from 'prosemirror-model';
+import { toggleMark } from 'prosemirror-commands';
 
 export type Keymap = (schema: Schema) => Record<string, Command>;
 
@@ -9,6 +10,9 @@ export function defaultKeymap(schema: Schema): Record<string, Command> {
   return {
     'Ctrl-z': undo,
     'Ctrl-Shift-z': redo,
+    'Ctrl-b': toggleMark(schema.marks['strong']),
+    'Ctrl-i': toggleMark(schema.marks['em']),
+    'Ctrl-u': toggleMark(schema.marks['underline']),
     Enter: splitListItem(schema.nodes.list_item),
   };
 }
