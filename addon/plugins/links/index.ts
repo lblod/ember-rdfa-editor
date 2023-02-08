2c8abf89265deb19519a30a18045eaeac80bacef
Oscar <lagartoverde97@gmail.com>
2023-02-08 12:36:02 +0100

    Handle ctrl click on links

---
+++ addon/plugins/links/index.ts
@@ -0,0 +1,25 @@
+import { PNode, ProsePlugin, RdfaEditorView } from '@lblod/ember-rdfa-editor';
+export const linkHandler: ProsePlugin = new ProsePlugin({
+  props: {
+    handleClickOn(
+      view: RdfaEditorView,
+      pos: number,
+      node: PNode,
+      nodePos: number,
+      event: MouseEvent
+    ) {
+      const schema = view.state.schema;
+      // The handler only handles clicks on nodes, and the link is a mark of the textnode inside the node, so we have to get it
+      const textNode = node.childAfter(pos);
+      if (!textNode || !textNode.node) return;
+      const linkMark = textNode.node.marks.find(
+        (mark) => mark.type === schema.marks['link']
+      );
+      if (linkMark) {
+        if (event.ctrlKey || event.metaKey) {
+          window.open(linkMark.attrs['href']);
+        }
+      }
+    },
+  },
+});
