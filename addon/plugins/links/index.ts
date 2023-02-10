8754a32927519689a8b266d92997525b12847640
Elena Poelman <elena.poelman@redpencil.io>
2023-02-10 14:43:48 +0100

    linting

--- addon/plugins/links/index.ts
+++ addon/plugins/links/index.ts
@@ -11,8 +11,8 @@ export const linkHandler: ProsePlugin = new ProsePlugin({
       const schema = view.state.schema;
       // The handler only handles clicks on nodes, and the link is a mark of the textnode inside the node, so we have to get it
       const textNode = node.childAfter(pos);
-      if (!textNode || !textNode.node) { 
-        return; 
+      if (!textNode || !textNode.node) {
+        return;
       }
       const linkMark = textNode.node.marks.find(
         (mark) => mark.type === schema.marks['link']
