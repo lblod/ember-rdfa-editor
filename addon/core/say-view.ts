6493afab04856a3af7a4971ccffb50994b9be325
abeforgit <arnebertrand@gmail.com>
2023-02-21 11:28:58 +0100

    fix(history): repair broken state tracking for views

--- addon/core/say-view.ts
+++ addon/core/say-view.ts
@@ -3,7 +3,7 @@ import { DirectEditorProps, EditorView } from 'prosemirror-view';
 import { tracked } from '@glimmer/tracking';
 
 export default class SayView extends EditorView {
-  @tracked trackedState: EditorState;
+  @tracked declare state: EditorState;
   @tracked parent?: SayView;
 
   constructor(
@@ -17,20 +17,7 @@ export default class SayView extends EditorView {
     props: DirectEditorProps,
     parent?: SayView
   ) {
-    super(place, {
-      ...props,
-      dispatchTransaction: (tr) => {
-        if (props.dispatchTransaction) {
-          props.dispatchTransaction(tr);
-          this.trackedState = this.state;
-        } else {
-          const newState = this.state.apply(tr);
-          this.trackedState = newState;
-          this.updateState(newState);
-        }
-      },
-    });
-    this.trackedState = this.state;
+    super(place, props);
     this.parent = parent;
   }
 }
