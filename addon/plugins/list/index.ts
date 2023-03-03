5851d43bb1c7cbb27dd74b24016c2995a3de2447
Elena Poelman <elena.poelman@redpencil.io>
2023-03-03 16:28:59 +0100

    export liftOutOfList in index.ts

--- addon/plugins/list/index.ts
+++ addon/plugins/list/index.ts
@@ -1,2 +1,3 @@
 export { toggleList } from './commands/toggle-list';
+export { liftOutOfList } from './commands/lift-out-of-list';
 export { ordered_list, list_item, bullet_list } from './nodes/list-nodes';
