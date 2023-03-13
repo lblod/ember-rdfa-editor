826324ccf17fa47e984f099213f82a9367bc9987
Elena Poelman <elena.poelman@redpencil.io>
2023-03-13 08:56:57 +0100

    feat(color-highlighting): make color-highlight menu use color-selector component

--- addon/components/plugins/text-style/highlight.ts
+++ addon/components/plugins/text-style/highlight.ts
@@ -2,7 +2,12 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { SayController } from '@lblod/ember-rdfa-editor';
-import { setHighlight } from '@lblod/ember-rdfa-editor/plugins/highlight';
+import {
+  clearHighlight,
+  setHighlight,
+} from '@lblod/ember-rdfa-editor/plugins/highlight';
+import { paintCycleHappened } from '@lblod/ember-rdfa-editor/utils/_private/editor-utils';
+import { modifier } from 'ember-modifier';
 
 type Args = {
   controller: SayController;
@@ -10,31 +15,50 @@ type Args = {
 };
 
 export default class HighlightMenu extends Component<Args> {
+  dropdownButton?: HTMLElement;
+
+  setupDropdownButton = modifier(
+    (element: HTMLElement) => {
+      this.dropdownButton = element;
+    },
+    { eager: false }
+  );
+  @tracked dropdownOpen = false;
   @tracked selectedColor = this.args.defaultColor;
 
   get controller() {
     return this.args.controller;
   }
 
-  get schema() {
-    return this.controller.schema;
-  }
-
-  get state() {
-    return this.controller.activeEditorState;
+  @action
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
   }
 
   @action
-  setHighlight(color: string) {
-    this.controller.doCommand(setHighlight(color));
-    this.controller.focus();
-    this.selectedColor = color;
+  async closeDropdown() {
+    this.dropdownOpen = false;
+    await paintCycleHappened();
+    this.args.controller.focus();
+  }
+
+  @action async clickOutsideDropdown(event: InputEvent) {
+    const isClosedByToggleButton = this.dropdownButton?.contains(
+      event.target as Node
+    );
+    if (!isClosedByToggleButton) {
+      await this.closeDropdown();
+    }
   }
 
   @action
-  onColorPickerChange(event: InputEvent) {
-    this.controller.focus();
-    const color = (event.target as HTMLInputElement).value;
-    this.setHighlight(color);
+  async setHighlight(color?: string) {
+    if (color) {
+      this.controller.doCommand(setHighlight(color));
+      this.selectedColor = color;
+    } else {
+      this.controller.doCommand(clearHighlight);
+    }
+    await this.closeDropdown();
   }
 }
--- addon/plugins/highlight/index.ts
+++ addon/plugins/highlight/index.ts
@@ -1,2 +1,3 @@
 export { highlight } from './marks/highlight';
 export { setHighlight } from './commands/set-highlight';
+export { clearHighlight } from './commands/clear-highlight';
