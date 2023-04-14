6a24c90d9ea7cdc769d960d5893eefc3d4e406ce
Elena Poelman <elena.poelman@redpencil.io>
2023-04-14 16:20:40 +0200

    add export of input-rules

--- addon/plugins/list/index.ts
+++ addon/plugins/list/index.ts
@@ -1,3 +1,4 @@
 export { toggleList } from './commands/toggle-list';
 export { ordered_list, list_item, bullet_list } from './nodes/list-nodes';
 export type { OrderListStyle } from './nodes/list-nodes';
+export { bullet_list_input_rule, ordered_list_input_rule } from './input_rules';
