0889d9488a57aec14e3767d8716fa0787bba10de
Elena Poelman <elena.poelman@redpencil.io>
2023-06-27 12:46:09 +0200

    update sample counter node to use new document language feature

--- tests/dummy/app/components/sample-ember-nodes/counter.ts
+++ tests/dummy/app/components/sample-ember-nodes/counter.ts
@@ -11,4 +11,12 @@ export default class InlineComponentsPluginCounter extends Component<EmberNodeAr
   get count() {
     return this.args.node.attrs.count as number;
   }
+
+  get label() {
+    if (this.args.controller.documentLanguage.startsWith('nl')) {
+      return 'Verhoog';
+    } else {
+      return 'Increase';
+    }
+  }
 }
