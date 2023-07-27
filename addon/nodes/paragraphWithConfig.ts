ba7c6466565a053033e639eb7b7d8b2fc70dab3e
Ruben <ruben.de.facq@redpencil.io>
2023-07-27 11:32:02 +0200

    allow overiding group in paragraphWithConfig

--- addon/nodes/paragraphWithConfig.ts
+++ addon/nodes/paragraphWithConfig.ts
@@ -4,11 +4,13 @@ import { paragraph } from './paragraph';
 interface paragraphConfig {
   content?: string;
   marks?: string;
+  group?: string;
 }
 
 export const paragraphWithConfig: (config?: paragraphConfig) => NodeSpec = ({
   content = paragraph.content,
   marks = paragraph.marks,
+  group = paragraph.group,
 } = {}) => {
-  return { ...paragraph, content: content, marks: marks };
+  return { ...paragraph, content: content, marks: marks, group: group };
 };
