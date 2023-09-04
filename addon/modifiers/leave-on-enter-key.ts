62f54d892a4780135986d8d2f33a17adfb17b6be
Ruben <ruben.de.facq@redpencil.io>
2023-09-04 10:21:47 +0200

    fix modifier positional arguments

--- addon/modifiers/leave-on-enter-key.ts
+++ addon/modifiers/leave-on-enter-key.ts
@@ -1,26 +1,27 @@
 import { modifier } from 'ember-modifier';
 import { SayController, TextSelection } from '..';
 
-export default modifier(function leaveOnEnterKey(
-  element: HTMLElement,
-  controller: SayController,
-  startPosNode: number,
-) {
-  const state = controller.mainEditorState;
-  const node = state.doc.resolve(startPosNode).nodeAfter;
-  const leaveOnEnter = (event: KeyboardEvent) => {
-    if (event !== 'Enter') return;
+export default modifier(
+  (
+    element: HTMLElement,
+    [controller, startPosNode]: [SayController, number],
+  ) => {
+    const state = controller.mainEditorState;
+    const node = state.doc.resolve(startPosNode).nodeAfter;
+    const leaveOnEnter = (event: KeyboardEvent) => {
+      if (event.key !== 'Enter' || !node) return;
 
-    const posAfter = startPosNode + node.nodeSize;
-    const tr = state.tr;
-    tr.setSelection(TextSelection.create(state.doc, posAfter));
-    controller.mainEditorView.dispatch(tr);
-    controller.mainEditorView.focus();
-  };
+      const posAfter = startPosNode + node.nodeSize;
+      const tr = state.tr;
+      tr.setSelection(TextSelection.create(state.doc, posAfter));
+      controller.mainEditorView.dispatch(tr);
+      controller.mainEditorView.focus();
+    };
 
-  element.addEventListener('keyup', leaveOnEnter);
+    element.addEventListener('keyup', leaveOnEnter);
 
-  return () => {
-    element.removeEventListener('keyup', leaveOnEnter);
-  };
-});
+    return () => {
+      element.removeEventListener('keyup', leaveOnEnter);
+    };
+  },
+);
