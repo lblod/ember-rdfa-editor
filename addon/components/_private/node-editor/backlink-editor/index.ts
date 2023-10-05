666fa7f9a04736c94b924a9e39d8cc97b121e6b1
Elena Poelman <elena.poelman@redpencil.io>
2023-10-05 15:09:42 +0200

    fix goToId in backlink-editor

--- addon/components/_private/node-editor/backlink-editor/index.ts
+++ addon/components/_private/node-editor/backlink-editor/index.ts
@@ -1,9 +1,12 @@
 import Component from '@glimmer/component';
 import SayController from '@lblod/ember-rdfa-editor/core/say-controller';
 import { goToNodeWithId } from '../utils/go-to-node-with-id';
+import { ResolvedNode } from '@lblod/ember-rdfa-editor/plugins/_private/editable-node';
+import { IncomingProp } from '@lblod/ember-rdfa-editor/core/say-parser';
 
 type Args = {
   controller: SayController;
+  node: ResolvedNode;
 };
 
 export default class BacklinkEditor extends Component<Args> {
@@ -11,5 +14,11 @@ export default class BacklinkEditor extends Component<Args> {
     return this.args.controller;
   }
 
-  goToNodeWithId = (id: string) => goToNodeWithId(id, this.controller);
+  get backlinks() {
+    return this.args.node.value.attrs.backlinks as Record<string, IncomingProp>;
+  }
+
+  goToNodeWithId = (id: string) => {
+    goToNodeWithId(id, this.controller);
+  };
 }
