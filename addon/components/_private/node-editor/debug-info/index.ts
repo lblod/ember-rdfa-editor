e216dd560049990d53bd89f4b85eb4c68dacf97b
Elena Poelman <elena.poelman@redpencil.io>
2023-10-09 11:48:59 +0200

    add node-type to debug-info component

--- addon/components/_private/node-editor/debug-info/index.ts
+++ addon/components/_private/node-editor/debug-info/index.ts
@@ -1,5 +1,15 @@
-import templateOnlyComponent from '@ember/component/template-only';
+import Component from '@glimmer/component';
+import { ResolvedNode } from '@lblod/ember-rdfa-editor/plugins/_private/editable-node';
 
-const DebugInfo = templateOnlyComponent();
+type Args = {
+  node: ResolvedNode;
+};
+export default class DebugInfo extends Component<Args> {
+  get pos() {
+    return this.args.node.pos;
+  }
 
-export default DebugInfo;
+  get nodeType() {
+    return this.args.node.value.type.name;
+  }
+}
