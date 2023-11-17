f94178f8f5b0401037eb9c66ff065cb3b05ba270
Elena Poelman <elena.poelman@redpencil.io>
2023-11-17 14:35:49 +0100

    add workarounds for deleting around inline ember-nodes in firefox and chrome plugins

--- addon/plugins/chrome-hacks-plugin/index.ts
+++ addon/plugins/chrome-hacks-plugin/index.ts
@@ -9,6 +9,28 @@ import { chrome } from '@lblod/ember-rdfa-editor/utils/_private/browser';
 export function chromeHacksPlugin(): ProsePlugin {
   const chromeHacksPlugin = new ProsePlugin({
     props: {
+      handleKeyDown(view, event: KeyboardEvent): boolean {
+        if (!chrome) {
+          return false;
+        }
+        if (event.key === 'Delete') {
+          const { $from, from, to } = view.state.selection;
+          if (from !== to) {
+            return false;
+          }
+          // The problem with deleting only occurs if we're at the start of the parent node
+          if ($from.parentOffset !== 0) {
+            return false;
+          }
+          const $posToCheck = view.state.doc.resolve($from.pos + 1);
+          const nodeAfter = $posToCheck.nodeAfter;
+          if (nodeAfter && nodeAfter.type.spec.needsChromeCursorFix) {
+            view.dispatch(view.state.tr.deleteRange(from, $posToCheck.pos));
+            return true;
+          }
+        }
+        return false;
+      },
       decorations(state: EditorState): DecorationSet | undefined {
         if (!chrome) {
           return;
