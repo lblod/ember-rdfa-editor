9d79b7c38b4a57fc04c9d1253e4d64ec9e16fa17
Elena Poelman <elena.poelman@redpencil.io>
2023-11-28 15:04:20 +0100

    addition of document-info toolbar pill

--- tests/dummy/app/components/sample-toolbar-responsive.ts
+++ tests/dummy/app/components/sample-toolbar-responsive.ts
@@ -1,6 +1,8 @@
 import Component from '@glimmer/component';
+import DocumentInfoPill from '@lblod/ember-rdfa-editor/components/_private/doc-editor/info-pill';
 import DocumentLanguagePill from '@lblod/ember-rdfa-editor/components/_private/doc-editor/lang-pill';
 
 export default class SampleToolbarResponsive extends Component {
   DocumentLanguagePill = DocumentLanguagePill;
+  DocumentInfoPill = DocumentInfoPill;
 }
