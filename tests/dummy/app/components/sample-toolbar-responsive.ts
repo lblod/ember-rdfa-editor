bcdd46c6ec1435cdc958fef15e8a707ac9864fc2
Elena Poelman <elena.poelman@redpencil.io>
2023-11-28 15:02:32 +0100

    add doc-lang pill

---
+++ tests/dummy/app/components/sample-toolbar-responsive.ts
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import DocumentLanguagePill from '@lblod/ember-rdfa-editor/components/_private/doc-editor/lang-pill';
+
+export default class SampleToolbarResponsive extends Component {
+  DocumentLanguagePill = DocumentLanguagePill;
+}
