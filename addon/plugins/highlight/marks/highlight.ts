cfaeba106fa82e903272bf7519e572b22f41d409
Deniss Kozickis <deniss.kozickis@gmail.com>
2023-12-21 17:06:44 +0200

    GN-4612: Paste highlight color correctly

--- addon/plugins/highlight/marks/highlight.ts
+++ addon/plugins/highlight/marks/highlight.ts
@@ -1,5 +1,16 @@
 import { MarkSpec } from 'prosemirror-model';
 
+/**
+ * Checks if the background attribute is just a color.
+ *
+ * - Hexadecimal color
+ * - RGB color
+ * - RGBA color
+ * - Named color
+ */
+const BACKGROUND_IS_SIMPLE_COLOR_REGEXP =
+  /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)|(^rgba\((\d{1,3},\s*){3}[\d.]+\)$)|(^rgb\((\d{1,3},\s*){2}[\d.]+\)$)|(^[a-z]+$)/i;
+
 export const highlight: MarkSpec = {
   attrs: {
     value: {},
@@ -8,6 +19,20 @@ export const highlight: MarkSpec = {
     {
       style: 'background-color',
       getAttrs(value) {
+        return {
+          value,
+        };
+      },
+    },
+    {
+      style: 'background',
+      getAttrs(value) {
+        if (typeof value !== 'string') return null;
+
+        const isColor = BACKGROUND_IS_SIMPLE_COLOR_REGEXP.test(value.trim());
+
+        if (!isColor) return null;
+
         return {
           value,
         };
