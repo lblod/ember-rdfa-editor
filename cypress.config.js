a6c904c878d71bc8621ea00b26655991feec1388
Deniss Kozickis <deniss.kozickis@gmail.com>
2024-01-30 14:48:04 +0200

    GN-4625: Add Cypress visual regression testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-undef */
 const { defineConfig } = require('cypress');
+const { configureVisualRegression } = require('cypress-visual-regression');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:4200',
+    env: {
+      visualRegression: {
+        type: 'regression',
+      },
+    },
+    screenshotsFolder: './cypress/snapshots/actual',
+    setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config);
+      configureVisualRegression(on);
+
+      return config;
     },
   },
 });
