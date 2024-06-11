d6c474da753457d88a73ff6b926ec54bce31b800
abeforgit <arnebertrand@gmail.com>
2024-06-11 17:55:05 +0200

    fix(lint): fix formatting

--- tests/dummy/app/components/sample-toolbar-responsive.ts
+++ tests/dummy/app/components/sample-toolbar-responsive.ts
@@ -4,13 +4,12 @@ import DocumentInfoPill from '@lblod/ember-rdfa-editor/components/_private/doc-e
 import DocumentLanguagePill from '@lblod/ember-rdfa-editor/components/_private/doc-editor/lang-pill';
 
 interface Args {
-  controller: SayController
-
+  controller: SayController;
 }
 export default class SampleToolbarResponsive extends Component<Args> {
   DocumentLanguagePill = DocumentLanguagePill;
   DocumentInfoPill = DocumentInfoPill;
   get supportsTables() {
-    return this.args.controller.activeEditorState.schema.nodes['table_cell']
+    return this.args.controller.activeEditorState.schema.nodes['table_cell'];
   }
 }
