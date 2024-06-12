38da57459bf4d1b7e1bd9c2c0d241f04476362cf
abeforgit <arnebertrand@gmail.com>
2024-06-12 23:13:22 +0200

    fix: don't mess with selections in the combinator

--- addon/utils/transaction-utils.ts
+++ addon/utils/transaction-utils.ts
@@ -36,25 +36,33 @@ export type TransactionMonad<R> = (
   state: EditorState,
 ) => TransactionMonadResult<R>;
 
+export interface TransactionCombinatorResult<R>
+  extends TransactionMonadResult<R[]> {
+  /**
+   * All the transactions that were applied in sequence to achieve this result, including any potential extra transactions from plugins.
+   * This allows calling code to inspect and use any non-document state that may have been lost
+   * such as selections, storedMarks and custom metadata
+   */
+  transactions: Transaction[];
+}
 /**
  * Builds a function that can chain {@link TransactionMonad} functions together.
  * It starts from the initial state given as the first parameter. The resulting combinator
- * then applies each monad function in sequence, ultimately generating one {@link TransactionMonadResult} which combines
+ * then applies each monad function in sequence, ultimately generating one {@link TransactionCombinatorResult} which combines
  * all operations.
  *
  * You can also optionally pass an initialtransaction, which is required to be made from the same state you pass as initialState.
  * This initial transaction will simply be processed before the monads are applied. This is handy in commands, where you usually
- * already have a selection and you don't realy want to be forced to write a monad function.
+ * already have a transaction and you don't realy want to be forced to write a monad function.
  *
  * The combination happens by appending all the steps of each transaction to an "accumulator" transaction, a bit similar to a
- * reduce function. There is also some logic to handle explicitly set selections, as they're a bit special since they assume to
- * be associated with the "doc" of a transaction, which is the document state after all the steps have run.
- * Here we map the selection of each transaction through all the steps (to re-associate it with the correct doc state), and then use that
- * as the selection for our accumulator. This means that later transactions clobber previous ones in terms of their selection, but there's no
- * way around that (since there can only ever be one selection).
+ * reduce function.
  *
- * This being said, it is still recommended to only mess with selections _after_ running the combinator, cause mapping selections is not
- * a perfect process.
+ * WARNING: the combinator does NOT carry over any selection changes, storedMarks changes, or metadata changes from the given transactions.
+ * The logic to correctly carry over selections was simply too complex. Instead, the return value carries the full array of transactions
+ * that were applied in sequence, so that calling code can restore any potentially lost state if needed.
+ * It is however recommended to write transactionMonads in such a way that they only deal with document transforms.
+ *
  * @param initialState the state to start from
  * @param initialTransaction optional initial transaction. If given, it must be made from the same state as you pass to initialState
  * @returns The resulting combinator
@@ -65,29 +73,36 @@ export function transactionCombinator<R>(
 ) {
   return function (
     transactionMonads: TransactionMonad<R>[],
-  ): TransactionMonadResult<R[]> {
+  ): TransactionCombinatorResult<R> {
     const tr = initialState.tr;
+    const appliedTransactions: Transaction[] = [];
     if (initialTransaction) {
       for (const step of initialTransaction.steps) {
         tr.step(step);
       }
-      if (initialTransaction.selectionSet) {
-        tr.setSelection(initialTransaction.selection.map(tr.doc, tr.mapping));
-      }
     }
-    let state = initialState.apply(tr);
+
+    const { state, transactions } = initialState.applyTransaction(tr);
+    let currentState = state;
+    appliedTransactions.push(...transactions);
     const results: R[] = [];
     for (const monad of transactionMonads) {
-      const { transaction, result } = monad(state);
-      state = state.apply(transaction);
+      const { transaction, result } = monad(currentState);
+      const { state, transactions } =
+        currentState.applyTransaction(transaction);
+      currentState = state;
+      appliedTransactions.push(...transactions);
+
       results.push(result);
       for (const step of transaction.steps) {
         tr.step(step);
       }
-      if (transaction.selectionSet) {
-        tr.setSelection(transaction.selection.map(tr.doc, tr.mapping));
-      }
     }
-    return { transaction: tr, result: results, initialState };
+    return {
+      transaction: tr,
+      result: results,
+      initialState,
+      transactions: appliedTransactions,
+    };
   };
 }
