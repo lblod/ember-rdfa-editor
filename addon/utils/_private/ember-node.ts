ea767fc28ecc9d0ce2890102abbe86ccd933b7cd
Oscar <lagartoverde97@gmail.com>
2024-08-16 13:46:09 +0200

    Add the ability to update an attribute without registering into the history

--- addon/utils/_private/ember-node.ts
+++ addon/utils/_private/ember-node.ts
@@ -184,10 +184,13 @@ class EmberNodeView implements NodeView {
       {
         getPos,
         node: pNode,
-        updateAttribute: (attr, value) => {
+        updateAttribute: (attr: string, value: any, ignoreHistory: boolean) => {
           const pos = getPos();
           if (pos !== undefined) {
             const transaction = view.state.tr;
+            if(ignoreHistory) {
+              transaction.setMeta('addToHistory', false)
+            }
             transaction.setNodeAttribute(pos, attr, value);
             view.dispatch(transaction);
           }
