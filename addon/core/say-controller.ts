bde2cba1bd67df27132ea1f87430cec716559822
abeforgit <arnebertrand@gmail.com>
2024-08-16 18:45:35 +0200

    test2

--- addon/core/say-controller.ts
+++ addon/core/say-controller.ts
@@ -24,7 +24,7 @@ export default class SayController {
 
   constructor(pm: SayEditor) {
     this.editor = pm;
-    console.log('hello');
+    console.log('hello2');
   }
 
   get externalContextStore(): SayStore {
