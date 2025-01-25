bbdac40be6893a7cf7ff0b75f349c0e95e6ec52b
abeforgit <arnebertrand@gmail.com>
2025-01-25 13:39:56 +0100

    fix(v2): use explicit reexports in index file

--- editor-v2/ember-rdfa-editor/src/index.ts
+++ editor-v2/ember-rdfa-editor/src/index.ts
@@ -1,4 +1,3 @@
-export * from 'prosemirror-keymap';
 import { isFullUri, isPrefixedUri } from '@lblod/marawa/rdfa-helpers';
 import { Decoration } from 'prosemirror-view';
 import { type CurieOptions, string } from 'yup';
@@ -6,11 +5,14 @@ import { addMethod } from 'yup';
 import { isNone } from './utils/_private/option.ts';
 
 import '#root/styles/ember-rdfa-editor.scss';
+export { keydownHandler, keymap } from 'prosemirror-keymap';
+
 export {
   getRdfaAttrs,
   rdfaAttrs,
   rdfaAttrSpec,
 } from '#root/core/schema.ts';
+
 export type { RdfaAttrs } from '#root/core/schema.ts';
 
 export {
@@ -18,17 +20,112 @@ export {
   type PluginConfig,
 } from '#root/core/say-editor.ts';
 
-export * from 'prosemirror-model';
+export type {
+  AttributeSpec,
+  Attrs,
+  DOMOutputSpec,
+  GenericParseRule,
+  MarkSpec,
+  NodeSpec,
+  ParseOptions,
+  ParseRule,
+  SchemaSpec,
+  StyleParseRule,
+  TagParseRule,
+} from 'prosemirror-model';
+
+export {
+  ContentMatch,
+  DOMParser,
+  DOMSerializer,
+  Fragment,
+  Mark,
+  MarkType,
+  Node,
+  NodeRange,
+  NodeType,
+  ReplaceError,
+  ResolvedPos,
+  Schema,
+  Slice,
+} from 'prosemirror-model';
 export { Node as PNode, DOMParser as ProseParser } from 'prosemirror-model';
 
-export * from 'prosemirror-state';
+export type {
+  Command,
+  EditorStateConfig,
+  PluginSpec,
+  PluginView,
+  SelectionBookmark,
+  StateField,
+} from 'prosemirror-state';
+
+export {
+  AllSelection,
+  EditorState,
+  NodeSelection,
+  Plugin,
+  PluginKey,
+  Selection,
+  SelectionRange,
+  TextSelection,
+  Transaction,
+} from 'prosemirror-state';
 export { Plugin as ProsePlugin } from 'prosemirror-state';
 
-export * from 'prosemirror-view';
+export type {
+  DecorationAttrs,
+  DecorationSource,
+  DirectEditorProps,
+  DOMEventMap,
+  EditorProps,
+  MarkViewConstructor,
+  NodeView,
+  NodeViewConstructor,
+} from 'prosemirror-view';
+export { Decoration, DecorationSet, EditorView } from 'prosemirror-view';
 
-export * from 'prosemirror-transform';
+export type { Mappable } from 'prosemirror-transform';
 
-export * from 'prosemirror-inputrules';
+export {
+  AddMarkStep,
+  AddNodeMarkStep,
+  AttrStep,
+  canJoin,
+  canSplit,
+  DocAttrStep,
+  dropPoint,
+  findWrapping,
+  insertPoint,
+  joinPoint,
+  liftTarget,
+  Mapping,
+  MapResult,
+  RemoveMarkStep,
+  RemoveNodeMarkStep,
+  ReplaceAroundStep,
+  replaceStep,
+  ReplaceStep,
+  Step,
+  StepMap,
+  StepResult,
+  Transform,
+} from 'prosemirror-transform';
+
+export {
+  closeDoubleQuote,
+  closeSingleQuote,
+  ellipsis,
+  emDash,
+  InputRule,
+  inputRules,
+  openDoubleQuote,
+  openSingleQuote,
+  smartQuotes,
+  textblockTypeInputRule,
+  undoInputRule,
+  wrappingInputRule,
+} from 'prosemirror-inputrules';
 
 export type InlineDecorationSpec = NonNullable<
   Parameters<typeof Decoration.inline>[3]
