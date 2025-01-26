be01ebaefc8daf82541500899aab05940cc18b4e
abeforgit <arnebertrand@gmail.com>
2025-01-26 01:10:08 +0100

    fix(v2): circular deps

--- editor-v2/ember-rdfa-editor/src/utils/_private/ce/paste-handler-helper-functions/cleanDocx.ts
+++ editor-v2/ember-rdfa-editor/src/utils/_private/ce/paste-handler-helper-functions/cleanDocx.ts
@@ -1,9 +1,7 @@
-import {
-  cleanListElements,
-  postCleanHtml,
-  cleanEmptyElements,
-  cleanLinkElements,
-} from '#root/utils/_private/ce/paste-handler-helper-functions/index.ts';
+import { cleanEmptyElements } from './cleanEmptyElements.ts';
+import { cleanLinkElements } from './cleanLinkElements.ts';
+import { cleanListElements } from './cleanListElements.ts';
+import { postCleanHtml } from './postCleanHtml.ts';
 
 export function cleanDocx(element: HTMLElement): string {
   cleanEmptyElements(element);
