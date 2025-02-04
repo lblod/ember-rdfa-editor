adaf871ca62a9a6852111d0ccadc6c9788aba6bd
abeforgit <arnebertrand@gmail.com>
2025-02-04 11:47:35 +0100

    fix(ci): get e2e test working

--- test-app/playwright.config.ts
+++ test-app/playwright.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
   ],
   // /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'pnpm start',
     url: 'http://127.0.0.1:4200',
     reuseExistingServer: !process.env['CI'],
   },
