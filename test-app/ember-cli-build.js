0f4ec7cde0c5fa8997d44d45a72446b7f059ef1e
Elena Poelman <elena.poelman@redpencil.io>
2025-03-19 11:34:13 +0100

    enable embroider and sourcemaps

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,18 +1,53 @@
 'use strict';
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const path = require('path');
+const fs = require('fs');
 
-module.exports = function (defaults) {
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+module.exports = async function (defaults) {
+  const { readPackageUpSync } = await import('read-package-up');
+
   let app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
-    autoImport: {
-      watchDependencies: ['@lblod/ember-rdfa-editor'],
-    },
+
     babel: {
       plugins: [
         require.resolve('ember-concurrency/async-arrow-task-transform'),
       ],
       sourceMaps: 'inline',
+    },
+    autoImport: {
+      watchDependencies: [],
+    },
+    trees: {
+      app: (() => {
+        let sideWatch = require('@embroider/broccoli-side-watch');
+
+        let paths = ['@lblod/ember-rdfa-editor'].map((libraryName) => {
+          let entry = require.resolve(libraryName);
+          let { packageJson, path: packageJsonPath } = readPackageUpSync({
+            cwd: entry,
+          });
+          let packagePath = path.dirname(packageJsonPath);
+
+          console.debug(
+            `Side-watching ${libraryName} from ${packagePath}, which started in ${entry}`,
+          );
+
+          let toWatch = packageJson.files
+            .map((f) => path.join(packagePath, f))
+            .filter((p) => {
+              if (!fs.existsSync(p)) return false;
+              if (!fs.lstatSync(p).isDirectory()) return false;
+
+              return !p.endsWith('/src');
+            });
+
+          return toWatch;
+        });
+
+        return sideWatch('app', { watching: paths.flat() });
+      })(),
     },
 
     sassOptions: {
@@ -25,5 +60,11 @@ module.exports = function (defaults) {
   });
 
   const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+  return maybeEmbroider(app, {
+    packagerOptions: {
+      webpackConfig: {
+        devtool: 'source-map',
+      },
+    },
+  });
 };
