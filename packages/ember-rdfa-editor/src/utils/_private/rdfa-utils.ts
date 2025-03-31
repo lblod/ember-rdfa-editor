3df71212602266a87e319375d76be6b46c829ccc
Elena Poelman <elena.poelman@redpencil.io>
2025-03-31 09:55:21 +0200

    addition of addBacklink transaction-monad

--- packages/ember-rdfa-editor/src/utils/_private/rdfa-utils.ts
+++ packages/ember-rdfa-editor/src/utils/_private/rdfa-utils.ts
@@ -1,5 +1,23 @@
+import type {
+  IncomingTriple,
+  OutgoingTriple,
+} from '#root/core/rdfa-processor.ts';
 import { getSubject } from '#root/plugins/rdfa-info/plugin.ts';
 import type { PNode } from '#root/prosemirror-aliases.ts';
+import type { EditorState } from 'prosemirror-state';
+import type {
+  TransactionMonad,
+  TransactionMonadResult,
+} from '../transaction-utils.ts';
+import {
+  addBacklink,
+  addProperty,
+  getNodeByRdfaId,
+  getNodesBySubject,
+} from '#root/plugins/rdfa-info/utils.ts';
+import { isRdfaAttrs } from '#root/core/schema.ts';
+import TransformUtils from './transform-utils.ts';
+import { sayDataFactory } from '#root/core/say-data-factory/data-factory.ts';
 
 export {
   addPropertyToNode,
@@ -42,4 +60,66 @@ export function getSubjects(node: PNode): Set<string> {
     return true;
   });
   return result;
+}
+
+export type AddBacklinkToNodeArgs = {
+  /** The resource to which to add a property */
+  rdfaId: string;
+  /** Property to add */
+  backlink: IncomingTriple;
+};
+export function addBacklinkToNode({
+  rdfaId,
+  backlink,
+}: AddBacklinkToNodeArgs): TransactionMonad<boolean> {
+  return function (state: EditorState): TransactionMonadResult<boolean> {
+    const doNothing = {
+      initialState: state,
+      transaction: state.tr,
+      result: false,
+    };
+    const node = getNodeByRdfaId(state, rdfaId);
+    if (!node || !isRdfaAttrs(node.value.attrs)) {
+      return doNothing;
+    }
+    const attrs = node.value.attrs;
+    const nodesToUpdate =
+      attrs.rdfaNodeType === 'resource'
+        ? getNodesBySubject(state, attrs.subject)
+        : [node];
+
+    const backlinks = node.value.attrs.backlinks;
+    const updatedBacklinks = addBacklink(backlinks, backlink);
+
+    const tr = state.tr;
+    nodesToUpdate.forEach((node) => {
+      TransformUtils.setAttribute(tr, node.pos, 'backlinks', updatedBacklinks);
+    });
+    const subjectNodes = getNodesBySubject(state, backlink.subject.value);
+    if (subjectNodes.length) {
+      const newProperty: OutgoingTriple =
+        attrs.rdfaNodeType === 'resource'
+          ? {
+              predicate: backlink.predicate,
+              object: sayDataFactory.resourceNode(attrs.subject),
+            }
+          : {
+              predicate: backlink.predicate,
+              object: sayDataFactory.literalNode(attrs.__rdfaId),
+            };
+      const properties = subjectNodes[0].value.attrs[
+        'properties'
+      ] as OutgoingTriple[];
+      const updatedProperties = addProperty(properties, newProperty);
+      subjectNodes.forEach((node) => {
+        TransformUtils.setAttribute(
+          tr,
+          node.pos,
+          'properties',
+          updatedProperties,
+        );
+      });
+    }
+    return { initialState: state, transaction: tr, result: true };
+  };
 }
