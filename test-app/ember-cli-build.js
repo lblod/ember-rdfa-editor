e3e446a4a2a01344f8baaadde409a21066402989
piemonkey <piemonkey@protonmail.ch>
2025-05-07 11:42:51 +0200

    Remove ember-changeset dependency
    This removes the peer dep on ember-data

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -7,15 +7,7 @@ const { compatBuild } = require('@embroider/compat');
 module.exports = async function (defaults) {
   const { buildOnce } = await import('@embroider/vite');
 
-  let app = new EmberApp(defaults, {
-    // We don't use ember-data or even depend on it directly, but it seems to get pulled in, even
-    // though it's an optional peer dependency of ember-changeset. This removes the deprecation.
-    emberData: {
-      deprecations: {
-        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
-      },
-    },
-  });
+  let app = new EmberApp(defaults, {});
 
   return compatBuild(app, buildOnce);
 };
