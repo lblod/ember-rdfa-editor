2e867a8836d77945bd69ba73efca0237d3e7fed1
piemonkey <piemonkey@protonmail.ch>
2026-01-21 10:31:02 +0100

    Remove test app to remove deps with warnings :/

--- test-app/e2e/types/window.d.ts
+++
@@ -1,9 +0,0 @@
-export {};
-
-declare global {
-  interface Window {
-    __PC: {
-      setHtmlContent: (content: string) => void;
-    };
-  }
-}
--- test-app/tests/helpers/common.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Promise which waits for ms milliseconds
- * @param ms number of milliseconds to wait
- * @returns A Promise which waits for ms milliseconds
- */
-export function delayMs(ms: number): Promise<void> {
-  return new Promise((r) => setTimeout(r, ms));
-}
