728c6f03ea6d53f9aed4a16be1af270432308c5b
piemonkey <piemonkey@protonmail.ch>
2026-02-04 12:11:03 +0100

    Add elements of readme quickstart to quickstart app

--- quickstart/app/app.ts
+++ quickstart/app/app.ts
@@ -5,6 +5,7 @@ import loadInitializers from 'ember-load-initializers';
 import config from 'quickstart/config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
+import './styles/app.scss';
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
