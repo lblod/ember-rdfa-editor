9201d34191a0fee0ae7b96ba5221cde058a244ea
piemonkey <piemonkey@protonmail.ch>
2026-06-24 17:29:55 +0200

    Add independent readme for plugin submodules

--- packages/say-ar-design-plugin/rollup.config.mjs
+++ packages/say-ar-design-plugin/rollup.config.mjs
@@ -144,12 +144,9 @@ export default [
       // Remove leftover build artifacts when starting a new build.
       addon.clean(),
 
-      // Copy Readme and License into published package
+      // Copy License into published package
       copy({
-        targets: [
-          { src: '../../README.md', dest: '.' },
-          { src: '../../LICENSE.md', dest: '.' },
-        ],
+        targets: [{ src: '../../LICENSE.md', dest: '.' }],
       }),
     ],
   },
